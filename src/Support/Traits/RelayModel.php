19f1fc441ddef70ba8bcc2a885fce6d66c81e778
Christopher Moore <chris@nuwavecommerce.com>
2016-05-31 08:02:38 -0700

    created trait to handle models w/ non id primary key

---
+++ src/Support/Traits/RelayModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Nuwave\Lighthouse\Support\Traits;
+
+trait RelayModel
+{
+    /**
+     * ID Attribute mutator.
+     *
+     * Note: Can be used if your Eloquent model doesn't
+     * have an id field.
+     *
+     * @return integer
+     */
+    public function getIdAttribute()
+    {
+        return $this->attributes[$this->getKeyName()];
+    }
+}
