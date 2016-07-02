34427e7cd483a172a9ec3baff19ebb648d5060ab
Christopher Moore <chris@nuwavecommerce.com>
2016-07-02 07:10:27 -0700

    use camel case for mutation name

--- src/Support/Console/Commands/MutationMakeCommand.php
+++ src/Support/Console/Commands/MutationMakeCommand.php
@@ -64,4 +64,20 @@ class MutationMakeCommand extends GeneratorCommand
             ['relay', null, InputOption::VALUE_NONE, 'Generate a Relay Mutation.'],
         ];
     }
+
+    /**
+     * Replace the class name for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     * @return string
+     */
+    protected function replaceClass($stub, $name)
+    {
+        $class = str_replace($this->getNamespace($name).'\\', '', $name);
+
+        $stub = str_replace('DummyCamelClass', camel_case($class), $stub);
+
+        return str_replace('DummyClass', $class, $stub);
+    }
 }
