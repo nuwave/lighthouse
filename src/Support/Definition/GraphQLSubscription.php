990b971a03cdfb347da0ec48e2388229939bde90
Christopher Moore <chris@nuwavecommerce.com>
2017-03-10 13:55:48 -0700

    change/add methods on subscription

--- src/Support/Definition/GraphQLSubscription.php
+++ src/Support/Definition/GraphQLSubscription.php
@@ -5,11 +5,21 @@ namespace Nuwave\Lighthouse\Support\Definition;
 abstract class GraphQLSubscription extends GraphQLQuery
 {
     /**
-     * Authorize subscription and create context.
+     * Determine if request is authorized to subscribe.
      *
      * @param  array $args
-     * @param  array $params
-     * @return mixed
+     * @param  mixed $request
+     * @param  \Illuminate\Support\Collection $context
+     * @return boolean
      */
-    abstract public function onSubscribe(array $args, array $params = []);
+    abstract public function canSubscribe(array $args, $request, $context);
+
+    /**
+     * Filter subscription.
+     *
+     * @param  array $args
+     * @param  \Illuminate\Support\Collection $context
+     * @return boolean
+     */
+    abstract public function filter(array $args, $context);
 }
