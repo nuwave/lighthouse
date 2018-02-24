243da2d8dd53bf0a46cd216b526732c470934942
Christopher Moore <chris@nuwavecommerce.com>
2018-02-24 15:51:04 -0700

    created placeholders for directives and schema

---
+++ src/Schema/Directives/Fields/FieldDirective.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nuwave\Lighthouse\Schema\Directives\Fields;
+
+// NOTE: Field directives can be resolvers or middleware
+abstract class FieldDirective
+{
+
+}
---
+++ src/Schema/Directives/Nodes/NodeDirective.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Nuwave\Lighthouse\Schema\Directives\Nodes;
+
+abstract class NodeDirective
+{
+    public function
+}
