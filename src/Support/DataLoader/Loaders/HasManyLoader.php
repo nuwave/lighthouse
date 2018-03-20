dcc19369ab0af926a9e4825bf7dac4ce43c92673
Christopher Moore <chris@nuwavecommerce.com>
2018-03-19 17:20:13 -0700

    prevent multiple calls to db

--- src/Support/DataLoader/Loaders/HasManyLoader.php
+++ src/Support/DataLoader/Loaders/HasManyLoader.php
@@ -3,9 +3,12 @@
 namespace Nuwave\Lighthouse\Support\DataLoader\Loaders;
 
 use Nuwave\Lighthouse\Support\DataLoader\BatchLoader;
+use Nuwave\Lighthouse\Support\Traits\HandlesGlobalId;
 
 class HasManyLoader extends BatchLoader
 {
+    use HandlesGlobalId;
+
     /**
      * Resolve keys.
      */
@@ -15,27 +18,34 @@ class HasManyLoader extends BatchLoader
             return array_merge($item, ['json' => json_encode($item['args'])]);
         })->groupBy('json')->each(function ($items) {
             $first = $items->first();
-            $relation = $first['relation'];
             $parents = $items->pluck('parent');
+            $scopes = array_get($first, 'scopes', []);
+            $relation = $first['relation'];
+            $type = $first['type'];
+            $args = $first['args'];
 
-            $parents->fetch([$relation => function ($q) use ($first) {
-                $args = $first['args'];
-                $type = $first['type'];
-                $scopes = array_get($first, 'scopes', []);
-
+            $constraints = [$relation => function ($q) use ($scopes, $args) {
                 foreach ($scopes as $scope) {
                     call_user_func_array([$q, $scope], [$args]);
                 }
+            }];
 
-                switch ($type) {
-                    case 'relay':
-                        return $q->relayConnection($args);
-                    case 'paginator':
-                        return $q->paginatorConnection($args);
-                    default:
-                        return $q;
-                }
-            }]);
+            switch ($type) {
+                case 'relay':
+                    $first = data_get($args, 'first', 15);
+                    $after = $this->decodeCursor($args);
+                    $currentPage = $first && $after ? floor(($first + $after) / $first) : 1;
+                    $parents->fetchForPage($first, $currentPage, $constraints);
+                    break;
+                case 'paginator':
+                    $first = data_get($args, 'count', 15);
+                    $page = data_get($args, 'page', 1);
+                    $parents->fetchForPage($first, $page, $constraints);
+                    break;
+                default:
+                    $parents->fetch($constraints);
+                    break;
+            }
 
             $parents->each(function ($model) use ($relation) {
                 $this->set($model->id, $model->getRelation($relation));
