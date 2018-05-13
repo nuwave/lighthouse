2981e2cfc8af2419b101070a4979e4cb9b4d3823
Oliver Nybroe <olivernybroe@gmail.com>
2018-05-13 21:11:39 +0200

    Feat: Added support for first directive.

---
+++ src/Schema/Directives/Fields/FirstDirective.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Nuwave\Lighthouse\Schema\Directives\Fields;
+
+
+use Nuwave\Lighthouse\Schema\Values\FieldValue;
+use Nuwave\Lighthouse\Support\Contracts\FieldResolver;
+use Nuwave\Lighthouse\Support\Traits\CanUseModels;
+use Nuwave\Lighthouse\Support\Traits\HandlesDirectives;
+use Nuwave\Lighthouse\Support\Traits\HandlesQueryFilter;
+
+class FirstDirective implements FieldResolver
+{
+    use CanUseModels, HandlesDirectives, HandlesQueryFilter;
+
+    /**
+     * Name of the directive.
+     *
+     * @return string
+     */
+    public function name()
+    {
+        return 'first';
+    }
+
+    /**
+     * Resolve the field directive.
+     *
+     * @param FieldValue $value
+     *
+     * @return FieldValue
+     * @throws \Nuwave\Lighthouse\Support\Exceptions\DirectiveException
+     */
+    public function resolveField(FieldValue $value)
+    {
+        $model = $this->getModelClass($value);
+
+        return $value->setResolver(function ($root, $args) use ($model) {
+            $query = $this->applyFiltersOnQuery($model::query(), $args);
+            return $query->first();
+        });
+    }
+}
