ee4e1acd6019825fc7f05a27ba1caa0a8705eb0a
spawnia <benedikt@franke.tech>
2018-06-08 11:58:54 +0200

    Expand group directive test

--- tests/Unit/Schema/Directives/Nodes/GroupDirectiveTest.php
+++ tests/Unit/Schema/Directives/Nodes/GroupDirectiveTest.php
@@ -9,31 +9,62 @@ class GroupDirectiveTest extends TestCase
     /**
      * @test
      */
-    public function itCanSetNamespaces()
+    public function itCanSetNamespaceThroughType()
     {
-        $schema = '
-        type Query {}
-        extend type Query @group(namespace: "Tests\\\Utils\\\Resolvers") {
+        $this->assertCanSetNamespace('
+        type Query @group(namespace: "Tests\\\Utils\\\Resolvers") {
             me: String @field(resolver: "Foo@bar")
         }
-        ';
-
-        $result = $this->execute($schema, '{ me }');
-        $this->assertEquals('foo.bar', $result->data['me']);
+        ');
     }
 
     /**
      * @test
      */
-    public function itCanSetMiddleware()
+    public function itCanSetNamespaceThroughTypeExtension()
     {
-        $schema = '
+        $this->assertCanSetNamespace('
+        type Query {}
+        extend type Query @group(namespace: "Tests\\\Utils\\\Resolvers") {
+            me: String @field(resolver: "Foo@bar")
+        }
+        ');
+    }
+
+    protected function assertCanSetNamespace($schema)
+    {
+        $result = $this->execute($schema, '{ me }');
+        $this->assertEquals('foo.bar', $result->data['me']);
+    }
+
+
+    /**
+     * @test
+     */
+    public function itCanSetMiddlewareThroughType()
+    {
+        $this->assertSetMiddleware('
+        type Query @group(middleware: ["foo", "bar"]) {
+            me: String @field(resolver: "Tests\\\Utils\\\Resolvers\\\Foo@bar")
+        }
+        ');
+    }
+
+    /**
+     * @test
+     */
+    public function itCanSetMiddlewareThroughTypeExtension()
+    {
+        $this->assertSetMiddleware('
         type Query {}
         extend type Query @group(middleware: ["foo", "bar"]) {
             me: String @field(resolver: "Tests\\\Utils\\\Resolvers\\\Foo@bar")
         }
-        ';
+        ');
+    }
 
+    protected function assertSetMiddleware($schema)
+    {
         $this->execute($schema, '{ me }');
         $middleware = graphql()->middleware()->query('me');
         $this->assertCount(2, $middleware);
