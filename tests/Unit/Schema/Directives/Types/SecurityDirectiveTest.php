735faa2b1b170b98fb492d58e9f1966721bfa3e2
spawnia <benedikt@franke.tech>
2018-06-14 17:30:24 +0200

    Split up Testsuites for Unit/Integration testing - This is because it is often useful to only run Unit tests first, since the Integration tests almost all fail if a single mistake happens - Found & fixed a bug where the static attributes on DocumentValidator, set in SecurityTest, influence the execution of all other tests

--- tests/Unit/Schema/Directives/Types/SecurityDirectiveTest.php
+++ tests/Unit/Schema/Directives/Types/SecurityDirectiveTest.php
@@ -14,14 +14,26 @@ class SecurityDirectiveTest extends TestCase
      */
     public function itCanSetMaxDepth()
     {
+        /** @var QueryDepth $ruleBefore */
+        $ruleBefore = DocumentValidator::getRule(QueryDepth::class);
+        $depthBefore = $ruleBefore->getMaxQueryDepth();
+
         $this->buildSchemaFromString('
             type Query @security(depth: 3) {
                 me: String
             }
         ');
 
-        $rule = DocumentValidator::getRule(QueryDepth::class);
-        $this->assertEquals(3, $rule->getMaxQueryDepth());
+        /** @var QueryDepth $rule */
+        $ruleAfter = DocumentValidator::getRule(QueryDepth::class);
+        DocumentValidator::defaultRules();
+        $depthAfter = $ruleAfter->getMaxQueryDepth();
+
+        $this->assertSame(3, $depthAfter);
+        $this->assertNotSame($depthBefore, $depthAfter);
+
+        // Reset this rule to allow tests following this to function normally
+        DocumentValidator::addRule($ruleBefore);
     }
 
     /**
@@ -29,13 +41,56 @@ class SecurityDirectiveTest extends TestCase
      */
     public function itCanSetMaxComplexity()
     {
+        /** @var QueryComplexity $ruleBefore */
+        $ruleBefore = DocumentValidator::getRule(QueryComplexity::class);
+        $complexityBefore = $ruleBefore->getMaxQueryComplexity();
+
         $this->buildSchemaFromString('
-        type Query @security(complexity: 3) {
-            me: String
-        }
+            type Query @security(complexity: 3) {
+                me: String
+            }
         ');
 
-        $rule = DocumentValidator::getRule(QueryComplexity::class);
-        $this->assertEquals(3, $rule->getMaxQueryComplexity());
+        /** @var QueryComplexity $ruleAfter */
+        $ruleAfter = DocumentValidator::getRule(QueryComplexity::class);
+        $complexityAfter = $ruleAfter->getMaxQueryComplexity();
+
+        $this->assertSame(3, $complexityAfter);
+        $this->assertNotSame($complexityBefore, $complexityAfter);
+
+        // Reset this rule to allow tests following this to function normally
+        DocumentValidator::addRule($ruleBefore);
+    }
+
+    /**
+     * @test
+     */
+    public function itCanDisableIntrospection()
+    {
+        $introspectionQuery = '
+            {
+                __schema {
+                    queryType {
+                        name
+                    }
+                }
+            } 
+        ';
+
+        $allowed = $this->execute('
+            type Query{
+                me: String
+            }
+        ', $introspectionQuery);
+        $this->assertAttributeEmpty('errors', $allowed);
+        $this->assertAttributeNotEmpty('data', $allowed);
+
+        $disallowed = $this->execute('
+            type Query @security(introspection: false){
+                me: String
+            }
+        ', $introspectionQuery);
+        $this->assertAttributeNotEmpty('errors', $disallowed);
+        $this->assertAttributeEmpty('data', $disallowed);
     }
 }
