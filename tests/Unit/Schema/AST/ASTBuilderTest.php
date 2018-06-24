2d7045dda987539ffa21f87b9db705bd838c8b12
Christopher Moore <chris@nuwavecommerce.com>
2018-06-24 14:59:03 -0700

    created immutable test

--- tests/Unit/Schema/AST/ASTBuilderTest.php
+++ tests/Unit/Schema/AST/ASTBuilderTest.php
@@ -3,11 +3,17 @@
 namespace Tests\Unit\Schema\AST;
 
 use Nuwave\Lighthouse\Schema\AST\ASTBuilder;
+use Nuwave\Lighthouse\Schema\AST\ASTHelper;
+use Nuwave\Lighthouse\Schema\AST\DocumentAST;
+use Nuwave\Lighthouse\Schema\AST\PartialParser;
 use Tests\TestCase;
 
 class ASTBuilderTest extends TestCase
 {
-    public function testMergeTypeExtensionFields()
+    /**
+     * @test
+     */
+    public function itCanMergeTypeExtensionFields()
     {
         $ast = ASTBuilder::generate('
             type Query {
@@ -24,5 +30,37 @@ class ASTBuilderTest extends TestCase
         ');
 
         $this->assertCount(3, $ast->objectType('Query')->fields);
+    }
+
+    /**
+     * @test
+     */
+    public function itCreatesAnImmutableDocumentAST()
+    {
+        $documentAST = ASTBuilder::generate('
+        type User {
+            email: String
+        }');
+
+        $originalDocument = new DocumentAST($documentAST->documentNode());
+
+        $userType = $documentAST->objectType('User');
+        $this->assertCount(1, $userType->fields);
+
+        $dummyType = PartialParser::objectTypeDefinition('
+        type DummyType {
+            name: String
+        }');
+
+        $userType->fields = ASTHelper::mergeNodeList(
+            $userType->fields,
+            $dummyType->fields
+        );
+
+        $this->assertCount(1, $documentAST->objectType('User')->fields);
+
+        $documentAST->setDefinition($userType);
+        $this->assertCount(2, $documentAST->objectType('User')->fields);
+        $this->assertCount(1, $originalDocument->objectType('User')->fields);
     }
 }
