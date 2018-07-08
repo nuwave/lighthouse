7b0992dace2f226eb074e1cbccb07296df19caa1
Christopher Moore <chris@nuwavecommerce.com>
2018-07-08 07:18:34 -0700

    create fallback and error for scalar class name

--- src/Schema/Resolvers/ScalarResolver.php
+++ src/Schema/Resolvers/ScalarResolver.php
@@ -3,8 +3,9 @@
 namespace Nuwave\Lighthouse\Schema\Resolvers;
 
 use GraphQL\Language\AST\DirectiveNode;
-use GraphQL\Language\AST\ScalarTypeDefinitionNode;
 use GraphQL\Type\Definition\ScalarType;
+use GraphQL\Language\AST\ScalarTypeDefinitionNode;
+use Nuwave\Lighthouse\Support\Exceptions\DirectiveException;
 
 class ScalarResolver extends AbstractResolver
 {
@@ -27,6 +28,18 @@ class ScalarResolver extends AbstractResolver
             ? $this->getClassName($directive)
             : ucfirst($this->value->getNodeName());
         $namespace = config('lighthouse.namespaces.scalars').'\\'.$className;
+
+        if (! class_exists($namespace) && class_exists($className)) {
+            $namespace = $className;
+        } elseif (! class_exists($namespace) && ! class_exists($className)) {
+            $message = sprintf(
+                'Unable to find class [%s] assigned to %s scalar',
+                $className,
+                $this->value->getNodeName()
+            );
+
+            throw new DirectiveException($message);
+        }
 
         return app($namespace);
     }
