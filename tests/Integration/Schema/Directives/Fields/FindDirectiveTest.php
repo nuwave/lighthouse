c4fb4223e8122efe2a01530ee3f62c5120704929
spawnia <benedikt@franke.tech>
2018-07-13 10:45:04 +0200

    Default to field type when no model argument is given
    Resolves #193

--- tests/Integration/Schema/Directives/Fields/FindDirectiveTest.php
+++ tests/Integration/Schema/Directives/Fields/FindDirectiveTest.php
@@ -1,109 +1,135 @@
 <?php
 
-
 namespace Tests\Integration\Schema\Directives\Fields;
 
-
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Nuwave\Lighthouse\Support\Exceptions\DirectiveException;
 use Tests\DBTestCase;
-use Tests\Utils\Models\Company;
 use Tests\Utils\Models\User;
+use Tests\Utils\Models\Company;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class FindDirectiveTest extends DBTestCase
 {
     use RefreshDatabase;
 
     /** @test */
-    public function can_return_single_user()
+    public function itReturnsSingleUser()
     {
+        $userA = factory(User::class)->create(['name' => 'A']);
+        $userB = factory(User::class)->create(['name' => 'B']);
+        $userC = factory(User::class)->create(['name' => 'C']);
+
         $schema = '
         type User {
             id: ID!
             name: String!
         }
+        
         type Query {
             user(id: ID @eq): User @find(model: "User")
         }
         ';
+        $query = "
+        {
+            user(id:{$userB->id}) {
+                name
+            }
+        }
+        ";
+        $result = $this->execute($schema, $query);
 
-        $userA = factory(User::class)->create(['name' => 'A']);
-        $userB = factory(User::class)->create(['name' => 'B']);
-        $userC = factory(User::class)->create(['name' => 'C']);
-
-
-        $result = $this->execute($schema, "{ user(id:{$userB->id}) { name } }");
         $this->assertEquals('B', $result->data['user']['name']);
     }
 
     /** @test */
-    public function can_fail_if_no_model_supplied()
+    public function itDefaultsToFieldTypeIfNoModelIsSupplied()
     {
+        $userA = factory(User::class)->create(['name' => 'A']);
+        $userB = factory(User::class)->create(['name' => 'B']);
+
         $schema = '
         type User {
             id: ID!
             name: String!
         }
+        
         type Query {
             user(id: ID @eq): User @find
         }
         ';
+        $query = "
+        {
+            user(id:{$userA->id}) {
+                name
+            }
+        }
+        ";
+        $result = $this->execute($schema, $query);
 
-        $userA = factory(User::class)->create(['name' => 'A']);
-        $userB = factory(User::class)->create(['name' => 'B']);
-        $userC = factory(User::class)->create(['name' => 'C']);
-
-
-        $this->expectException(DirectiveException::class);
-        $result = $this->execute($schema, "{ user(id:{$userA->id}) { name } }");
+        $this->assertEquals('A', $result->data['user']['name']);
     }
 
     /** @test */
-    public function cannot_fetch_if_multiple_models_match()
+    public function itCannotFetchIfMultipleModelsMatch()
     {
-        $schema = '
-        type User {
-            id: ID!
-            name: String!
-        }
-        type Query {
-            user(name: String @eq): User @find(model: "User")
-        }
-        ';
-
         $userA = factory(User::class)->create(['name' => 'A']);
         $userB = factory(User::class)->create(['name' => 'A']);
         $userC = factory(User::class)->create(['name' => 'B']);
 
-
-        $result = $this->execute($schema, "{ user(name: \"A\") { name } }");
-        $this->assertCount(1, $result->errors);
-    }
-
-    /** @test */
-    public function can_use_scopes()
-    {
         $schema = '
-        type Company {
-            name: String!
-        }
         type User {
             id: ID!
             name: String!
         }
+        
         type Query {
-            user(name: String @eq, company: String!): User @find(model: "User" scopes: [companyName])
+            user(name: String @eq): User @find(model: "User")
         }
         ';
+        $query = '
+        {
+            user(name: "A") {
+                name
+            }
+        }
+        ';
+        $result = $this->execute($schema, $query);
 
+        $this->assertCount(1, $result->errors);
+    }
+
+    /** @test */
+    public function itCanUseScopes()
+    {
         $companyA = factory(Company::class)->create(['name' => 'CompanyA']);
         $companyB = factory(Company::class)->create(['name' => 'CompanyB']);
         $userA = factory(User::class)->create(['name' => 'A', 'company_id' => $companyA->id]);
         $userB = factory(User::class)->create(['name' => 'A', 'company_id' => $companyB->id]);
         $userC = factory(User::class)->create(['name' => 'B', 'company_id' => $companyA->id]);
 
+        $schema = '
+        type Company {
+            name: String!
+        }
+        
+        type User {
+            id: ID!
+            name: String!
+        }
+        
+        type Query {
+            user(name: String @eq, company: String!): User @find(model: "User" scopes: [companyName])
+        }
+        ';
+        $query = '
+        {
+            user(name: "A" company: "CompanyA") {
+                id
+                name
+            }
+        }
+        ';
+        $result = $this->execute($schema, $query);
 
-        $result = $this->execute($schema, "{ user(name: \"A\" company: \"CompanyA\") { id, name } }");
         $this->assertEquals($userA->id, $result->data['user']['id']);
         $this->assertEquals('A', $result->data['user']['name']);
     }
