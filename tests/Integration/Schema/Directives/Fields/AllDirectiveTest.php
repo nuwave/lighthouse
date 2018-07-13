1a3a1cb5bd2fa6f683e3e368323bfb5dd2bdec57
spawnia <benedikt@franke.tech>
2018-07-13 16:03:12 +0200

    Change AllDirectives test method

--- tests/Integration/Schema/Directives/Fields/AllDirectiveTest.php
+++ tests/Integration/Schema/Directives/Fields/AllDirectiveTest.php
@@ -39,7 +39,7 @@ class AllDirectiveTest extends DBTestCase
         }
         ';
 
-        $result = $this->execute($schema, $query);
+        $result = $this->queryAndReturnResult($schema, $query);
         $this->assertCount(10, $result->data['users']);
     }
 
@@ -78,7 +78,7 @@ class AllDirectiveTest extends DBTestCase
         }
         ';
 
-        $result = $this->execute($schema, $query);
+        $result = $this->queryAndReturnResult($schema, $query);
         $this->assertCount(1, array_get($result->data, 'users'));
         $this->assertCount(10, array_get($result->data, 'users.0.posts'));
     }
