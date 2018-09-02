ed07060e233721750d701bc1582e55ed7bff18c2
spawnia <benedikt@franke.tech>
2018-09-02 20:59:25 +0200

    Make the inject directive use the User id, test failing as it runs out of memory

--- tests/Unit/Schema/Directives/Fields/InjectDirectiveTest.php
+++ tests/Unit/Schema/Directives/Fields/InjectDirectiveTest.php
@@ -2,22 +2,18 @@
 
 namespace Tests\Unit\Schema\Directives\Fields;
 
-use Tests\TestCase;
-use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\User;
-use Nuwave\Lighthouse\Schema\Context;
-use GraphQL\Type\Definition\ResolveInfo;
+use Tests\DBTestCase;
+use Tests\Utils\Models\User;
 
-class InjectDirectiveTest extends TestCase
+class InjectDirectiveTest extends DBTestCase
 {
     /**
      * @test
      */
     public function itCanInjectDataFromContextIntoArgs()
     {
-        $user = new class() extends User {
-            public $foo = 'bar';
-        };
+        $user = factory(User::class)->create();
+        $this->be($user);
 
         $schema = $this->buildSchemaFromString('
         type User {
@@ -25,17 +21,23 @@ class InjectDirectiveTest extends TestCase
         }
         
         type Query {
-            user: User! @inject(context: "user.foo", name: "foo") @field(resolver: "' . addslashes(self::class) . '@resolveUser")
+            user: User! @inject(context: "user.id", name: "user_id") @field(resolver: "' . addslashes(self::class) . '@resolveUser")
         }
         ');
 
-        $query = $schema->getType('Query');
-        $resolver = array_get($query->config['fields'](), 'user.resolve');
-        $resolver(null, [], new Context(new Request([]), $user), new ResolveInfo([]));
+        $query = '
+        {
+            user {
+                foo
+            }
+        }
+        ';
+
+        $this->postJson('graphql',['query' => $query]);
     }
 
     public function resolveUser($root, $args)
     {
-        $this->assertSame('bar', $args['foo']);
+        $this->assertSame(1, $args['user_id']);
     }
 }
