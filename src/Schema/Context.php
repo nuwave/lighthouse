7d04088ea8c5d3971726825b19a898089dc3b4db
Benedikt Franke <benedikt@franke.tech>
2018-09-05 20:06:57 +0200

    Allow the $user property of Context to be null

--- src/Schema/Context.php
+++ src/Schema/Context.php
@@ -3,6 +3,7 @@
 namespace Nuwave\Lighthouse\Schema;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
 
 class Context
 {
@@ -14,12 +15,23 @@ class Context
     public $request;
 
     /**
+     * Authenticated user.
+     *
+     * May be null since some fields may be accessible without authentication.
+     *
+     * @var User|null
+     */
+    public $user;
+
+    /**
      * Create new context.
      *
      * @param Request $request
+     * @param User|null $user
      */
-    public function __construct(Request $request)
+    public function __construct(Request $request, User $user = null)
     {
         $this->request = $request;
+        $this->user = $user;
     }
 }
