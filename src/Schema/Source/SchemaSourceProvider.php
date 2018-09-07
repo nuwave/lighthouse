106d05db170d2df90a401d97c5accd330e7e1e8d
Christopher Moore <chris@nuwavecommerce.com>
2018-09-07 13:38:01 -0700

    remove return types on interfaces

--- src/Schema/Source/SchemaSourceProvider.php
+++ src/Schema/Source/SchemaSourceProvider.php
@@ -3,13 +3,7 @@
 namespace Nuwave\Lighthouse\Schema\Source;
 
 /**
- * Interface SchemaSourceProvider
- * @package Nuwave\Lighthouse\Schema\Source
- *
- * This interface can be implemented to have different methods of providing
- * a schema string. Since the schema is then parsed into an AST and cached,
- * this does not necessarily have to be very performant, e.g. it may be
- * acceptable to download the schema or fetch it from an API.
+ * Interface SchemaSourceProvider.
  */
 interface SchemaSourceProvider
 {
@@ -18,5 +12,5 @@ interface SchemaSourceProvider
      *
      * @return string
      */
-    public function getSchemaString(): string;
+    public function getSchemaString();
 }
