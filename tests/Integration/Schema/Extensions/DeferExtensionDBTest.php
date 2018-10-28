39506a031178650534c2473de1b7ac7022e22b95
Christopher Moore <christophermoore@Christophers-MacBook-Pro-2.local>
2018-10-28 13:04:53 -0700

    created test for nested list fields

--- tests/Integration/Schema/Extensions/DeferExtensionDBTest.php
+++ tests/Integration/Schema/Extensions/DeferExtensionDBTest.php
@@ -171,6 +171,96 @@ class DeferExtensionDBTest extends DBTestCase
         );
     }
 
+    /**
+     * @test
+     */
+    public function itCanDeferNestedListFields()
+    {
+        $queries = 0;
+        $resolver = addslashes(self::class).'@resolve';
+        $companies = factory(Company::class, 2)
+            ->create()
+            ->each(function (Company $company) {
+                factory(User::class, 3)->create([
+                    'company_id' => $company->getKey(),
+                ]);
+            });
+
+        self::$resolver = function () use ($companies) {
+            return $companies;
+        };
+
+        $this->schema = "
+        type Company {
+            name: String!
+            users: [User] @hasMany
+        }
+
+        type User {
+            email: String!
+            company: Company @belongsTo
+        }
+
+        type Query {
+            companies: [Company] @field(resolver: \"{$resolver}\")
+        }";
+
+        $query = '
+        {
+            companies {
+                name
+                users @defer {
+                    email
+                    company @defer {
+                        name
+                    }
+                }
+            }
+        }';
+
+        \DB::listen(function ($q) use (&$queries) {
+            ++$queries;
+        });
+
+        $this->postJson('/graphql', compact('query'))
+            ->baseResponse
+            ->send();
+
+        $chunks = $this->stream->chunks;
+        $this->assertEquals(2, $queries);
+        $this->assertCount(3, $chunks);
+
+        $deferredCompanies = $chunks[0];
+        $this->assertEquals($companies[0]->name, array_get($deferredCompanies, 'data.companies.0.name'));
+        $this->assertEquals($companies[1]->name, array_get($deferredCompanies, 'data.companies.1.name'));
+        $this->assertNull(array_get($deferredCompanies, 'data.companies.0.users'));
+        $this->assertNull(array_get($deferredCompanies, 'data.companies.1.users'));
+
+        $deferredUsers = $chunks[1];
+        $companies->each(function ($company, $i) use ($deferredUsers) {
+            $key = "companies.{$i}.users";
+            $this->assertArrayHasKey($key, $deferredUsers);
+            $this->assertEquals(
+                $company->users->map(function ($user) {
+                    return [
+                        'email' => $user->email,
+                        'company' => null,
+                    ];
+                })->toArray(),
+                $deferredUsers[$key]
+            );
+        });
+
+        $deferredCompanies = $chunks[2];
+        $this->assertCount(6, $deferredCompanies);
+        collect($deferredCompanies)->each(function ($item) use ($companies) {
+            $this->assertArrayHasKey('name', $item);
+            $this->assertTrue(
+                in_array($item['name'], $companies->pluck('name')->all())
+            );
+        });
+    }
+
     public function resolve()
     {
         $resolver = self::$resolver;
