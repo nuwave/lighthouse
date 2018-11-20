9c3928e455cfda4dd5c3bf386dabcc077e446e89
Robson Tenório <robsontenorio@MacBook-Pro-de-Robson.local>
2018-11-19 22:20:51 -0200

    Finish versioning mechanism

--- docs/.vuepress/lib/versioning.js
+++ docs/.vuepress/lib/versioning.js
@@ -1,10 +1,11 @@
 // TIP this file not work with hot reloading. You must restart server.
 
-// TODO
-//   - expcetion if version exists
-//   - mandatory argument
+
 
 const versions = require('../versions.json')
+const fse = require('fs-extra')
+const path = process.cwd()
+
 
 module.exports = {
   versions: {
@@ -15,6 +16,8 @@ module.exports = {
       return versions
     }
   },
+  // Generate a single object that represents all versions from each sidebar
+  // https://vuepress.vuejs.org/theme/default-theme-config.html#multiple-sidebars
   get sidebars () {
     let sidebars = {}
 
@@ -25,6 +28,7 @@ module.exports = {
 
     return sidebars
   },
+  // Build dropdown items for each version  
   linksFor (url) {
     let links = []
 
@@ -34,6 +38,51 @@ module.exports = {
     })
 
     return links
+  },
+  // Generate a new version
+  generate (version) {
+    version = version || process.argv[1]
+    console.log('\n')
+
+    if (!fs.existsSync(`${path}/.vuepress/versions.json`)) {
+      this.error('File .vupress/versions.json not found')
+    }
+
+    if (typeof version === 'undefined') {
+      this.error('No version number specified! \nPass the version you wish to create as an argument.\nEx: 1.0.0')
+    }
+
+    if (versions.includes(version)) {
+      this.error(`This version '${version}' already exists! Specify a new version to create that does not already exist.`)
+    }
+
+    this.info(`Generating new version into 'docs/${version}' ...`)
+
+    try {
+      fse.copySync(`${path}/master`, `${path}/${version}`)
+
+      // update versions.json file
+      versions.unshift(version)
+
+      fs.writeFileSync(
+        `${path}/.vuepress/versions.json`,
+        `${JSON.stringify(versions, null, 2)}\n`,
+      );
+
+      this.success(`Version '${version}' created!`)
+    } catch (e) {
+      this.error(e)
+    }
+  },
+  error (message) {
+    console.log("\x1b[41m%s\x1b[0m", ' ERROR ', `${message}\n`)
+    process.exit(0)
+  },
+  info (message) {
+    console.log("\x1b[44m%s\x1b[0m", ' INFO ', `${message}\n`)
+  },
+  success (message) {
+    console.log("\x1b[42m\x1b[30m%s\x1b[0m", ' DONE ', `${message}\n`)
   }
 }
 
