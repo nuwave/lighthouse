c5d8a2c4a640aa6ac309e6f5c9dd0921c718b5c6
Christopher Moore <chris@nuwavecommerce.com>
2018-12-02 11:48:39 -0700

    force rebuild of footer

--- docs/packages/vuepress-theme-lighthouse/components/Footer.vue
+++ docs/packages/vuepress-theme-lighthouse/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="flex text-grey-dark text-sm">
       <div class="flex-1 text-center">
         <p class="text-grey-darker mb-4">Copyright © 2018 Christopher Moore</p>
-        <p class="mb-0">
+        <p>
           Made with
           <span class="text-xs">❤️</span> by the
           <a
