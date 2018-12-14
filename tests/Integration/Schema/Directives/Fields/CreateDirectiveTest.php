e8adf6bd556d6e43b33ae815bad2574bc6ef72d5
spawnia <benedikt@franke.tech>
2018-12-14 16:28:12 +0100

    Add a test for the create directive nested has many

--- tests/Integration/Schema/Directives/Fields/CreateDirectiveTest.php
+++ tests/Integration/Schema/Directives/Fields/CreateDirectiveTest.php
@@ -119,4 +119,66 @@ class CreateDirectiveTest extends DBTestCase
         $this->assertSame('foo', Arr::get($result, 'data.createTask.name'));
         $this->assertSame('1', Arr::get($result, 'data.createTask.user.id'));
     }
+
+    /**
+     * @test
+     */
+    public function itCanCreateWithHasMany()
+    {
+        $schema = '
+        type Task {
+            id: ID!
+            name: String!
+        }
+        
+        type User {
+            id: ID!
+            name: String
+            tasks: [Task!]! @hasMany
+        }
+        
+        type Mutation {
+            createUser(input: CreateUserInput!): User @create(flatten: true)
+        }
+        
+        input CreateUserInput {
+            name: String
+            tasks: CreateTaskRelation
+        }
+        
+        input CreateTaskRelation {
+            create: [CreateTaskInput!]
+        }
+        
+        input CreateTaskInput {
+            name: String
+            user: ID
+        }
+        ' . $this->placeholderQuery();
+        $query = '
+        mutation {
+            createUser(input: {
+                name: "foo"
+                tasks: {
+                    create: [{
+                        name: "bar"
+                    }]
+                }
+            }) {
+                id
+                name
+                tasks {
+                    id
+                    name
+                }
+            }
+        }
+        ';
+        $result = $this->execute($schema, $query);
+
+        $this->assertSame('1', Arr::get($result, 'data.createUser.id'));
+        $this->assertSame('foo', Arr::get($result, 'data.createUser.name'));
+        $this->assertSame('1', Arr::get($result, 'data.createUser.tasks.0.id'));
+        $this->assertSame('bar', Arr::get($result, 'data.createUser.tasks.0.name'));
+    }
 }
