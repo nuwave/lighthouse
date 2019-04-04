fccbab7344fb4620a3a813db4885ae7b30325ecf
Bram Wubs <b.wubs@code14.nl>
2019-04-04 22:15:54 +0200

    Add MorphToManyTest

---
+++ tests/Integration/Execution/MutationExecutor/MorphToManyTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\Integration\Execution\MutationExecutor;
+
+use Tests\DBTestCase;
+use Tests\Utils\Models\Tag;
+
+class MorphToManyTest extends DBTestCase
+{
+    protected $schema = '
+        type Task {
+            id: ID!
+            name: String!
+            tags: [Tag!]!
+        }
+        
+        type Tag {
+            id: ID!
+            name: String!
+        }
+        
+        input CreateTagRelation {
+            sync: [ID!]
+            connect: [ID!]
+        }
+        
+        input CreateTaskInput {
+            name: String!
+            tags: CreateTagRelation
+        }
+        
+        type Mutation {
+            createTask(input: CreateTaskInput!): Task @create(flatten: true)
+        }
+    ';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->schema .= $this->placeholderQuery();
+    }
+
+    /**
+     * @test
+     */
+    public function itCanCreateATaskWithExistingTagsByUsingConnect(){
+        $id = factory(Tag::class)->create(['name' => 'php'])->id;
+
+        $this->query('
+            mutation {
+            createTask(input: {
+                    name: "Finish tests"
+                    tags: {
+                        connect: [1]
+                    }
+                }){
+                    tags{
+                            id
+                        }
+                    }
+                }
+            ')->assertJson([
+                'data'=> [
+                    'createTask' => [
+                        'tags' => [
+                            [
+                            'id' => $id
+                            ]
+                        ]
+                    ]
+                ]
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function itCanCreateATaskWithExistingTagsByUsingSync(){
+        $id = factory(Tag::class)->create(['name' => 'php'])->id;
+
+        $this->query('
+            mutation {
+            createTask(input: {
+                    name: "Finish tests"
+                    tags: {
+                        sync: [1]
+                    }
+                }){
+                    tags{
+                            id
+                        }
+                    }
+                }
+            ')->assertJson([
+            'data'=> [
+                'createTask' => [
+                    'tags' => [
+                        [
+                            'id' => $id
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
