e3692b44de13c21a324c1266c914aa7eaa7a7dbf
spawnia <benedikt@franke.tech>
2019-04-15 09:08:31 +0200

    Add deprecation notices to between directives

--- src/Between/WhereBetweenDirective.php
+++ src/Between/WhereBetweenDirective.php
@@ -20,6 +20,8 @@ class WhereBetweenDirective extends BaseDirective implements ArgBuilderDirective
     }
 
     /**
+     * Apply a "WHERE BETWEEN" clause.
+     *
      * @param  \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder  $builder
      * @param  mixed  $values
      * @return \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder
--- src/Between/WhereNotBetweenDirective.php
+++ src/Between/WhereNotBetweenDirective.php
@@ -20,6 +20,8 @@ class WhereNotBetweenDirective extends BaseDirective implements ArgBuilderDirect
     }
 
     /**
+     * Apply a "WHERE NOT BETWEEN" clause.
+     *
      * @param  \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder  $builder
      * @param  mixed  $values
      * @return \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder
