472f62a7529e7a93a5d0adae7b33e73ec81da8e1
Lucas Michot <lucas@semalead.com>
2019-04-18 10:33:48 +0200

    Reformat.

--- src/Support/Http/routes.php
+++ src/Support/Http/routes.php
@@ -4,9 +4,9 @@ app('router')->group(config('lighthouse.route', []), function (): void {
     $routeName = config('lighthouse.route_name', 'graphql');
     $controller = config('lighthouse.controller');
 
-    $methods = config('lighthouse.route_enable_get', false) ?
-        ['GET', 'POST'] :
-        ['POST'];
+    $methods = config('lighthouse.route_enable_get', false)
+        ? ['GET', 'POST']
+        : ['POST'];
 
     app('router')->match($methods, $routeName, [
         'as' => 'lighthouse.graphql',
