4991036f571bc79f0a30580eb40e7f42a659c5c7
spawnia <benedikt@franke.tech>
2019-04-18 21:02:20 +0200

    add comment

--- src/Support/Http/Middleware/AcceptJson.php
+++ src/Support/Http/Middleware/AcceptJson.php
@@ -6,6 +6,13 @@ namespace Nuwave\Lighthouse\Support\Http\Middleware;
 
 use Illuminate\Http\Request;
 
+/**
+ * Always set the Accept: application/json header.
+ *
+ * This makes it easier to do simple GET requests from clients
+ * with limited HTTP configuration options and always receive
+ * a proper result, even in case of unforeseen errors.
+ */
 class AcceptJson
 {
     public function handle(Request $request, \Closure $next)
