feffb5b971b540c330b278d9e254c7cd8098933f
spawnia <benedikt@franke.tech>
2019-05-04 20:22:21 +0200

    Add phpbench and a benchmark for ASTUnserialization

---
+++ benchmarks/ASTUnserializationBench.php
@@ -0,0 +1,71 @@
+<?php
+
+use GraphQL\Language\Parser;
+
+
+/**
+ * @BeforeMethods({"prepareSchema"})
+ */
+class ASTUnserializationBench
+{
+    const SCHEMA = /** @lang GraphQL */
+        <<<SCHEMA
+type Query {
+  query1: String
+  query2: String
+}
+
+type Mutation {
+  mutation1: Int
+  mutation2: Int
+}
+
+type Foo {
+  foo1: Boolean
+  foo2: Boolean
+}
+SCHEMA;
+
+    /**
+     * @var string
+     */
+    private $documentNode;
+
+    /**
+     * @var string
+     */
+    private $documentAST;
+
+    public function prepareSchema(): void
+    {
+        $this->documentNode = serialize(
+            Parser::parse(
+                self::SCHEMA,
+                // Ignore location since it only bloats the AST
+                ['noLocation' => true]
+            )
+        );
+
+        $this->documentAST = serialize(
+            \Nuwave\Lighthouse\Schema\AST\DocumentAST::fromSource(self::SCHEMA)
+        );
+    }
+
+    /**
+     * @Revs(100)
+     * @Iterations(10)
+     */
+    public function benchUnserializeDocumentNode(): void
+    {
+        unserialize($this->documentNode);
+    }
+
+    /**
+     * @Revs(100)
+     * @Iterations(10)
+     */
+    public function benchUnserializeDocumentAST(): void
+    {
+        unserialize($this->documentAST);
+    }
+}
