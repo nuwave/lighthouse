af3370d6628e9af0748aa52361ac2bd3b445dbf2
Adrian Gligor <adrian.gligor@gmail.com>
2019-05-14 16:50:25 +0200

    Make type hint more specific

--- src/Support/Compatibility/MiddlewareBridge.php
+++ src/Support/Compatibility/MiddlewareBridge.php
@@ -7,14 +7,14 @@ interface MiddlewareBridge
     /**
      * Get all of the defined middleware short-hand names.
      *
-     * @return array
+     * @return string[]
      */
     public function getMiddleware(): array;
 
     /**
      * Get all of the defined middleware groups.
      *
-     * @return array
+     * @return string[]
      */
     public function getMiddlewareGroups(): array;
 }
