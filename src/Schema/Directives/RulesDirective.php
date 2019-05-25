3fe863b7cce442fdb0af53c947e2b1b2c0692a2e
Omar <oomarusman@gmail.com>
2019-05-25 15:56:12 +0800

    Support custom rules.

--- src/Schema/Directives/RulesDirective.php
+++ src/Schema/Directives/RulesDirective.php
@@ -3,6 +3,7 @@
 namespace Nuwave\Lighthouse\Schema\Directives;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\App;
 use Nuwave\Lighthouse\Support\Contracts\ArgValidationDirective;
 use Nuwave\Lighthouse\Support\Traits\HasArgumentPath as HasArgumentPathTrait;
 use Nuwave\Lighthouse\Support\Contracts\HasArgumentPath as HasArgumentPathContract;
@@ -27,6 +28,14 @@ class RulesDirective extends BaseDirective implements ArgValidationDirective, Ha
     public function getRules(): array
     {
         $rules = $this->directiveArgValue('apply');
+
+        // Resolve custom rule namespace, if possible.
+        foreach ($rules as $key => $rule) {
+
+            if (strpos($rule, 'App\Rules') === 0) {
+                $rules[$key] = App::make($rule);
+            }
+        }
 
         return [$this->argumentPathAsDotNotation() => $rules];
     }
