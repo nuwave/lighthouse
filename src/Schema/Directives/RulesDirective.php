775ebba4b97d16dc5aa2710946f4c0e8b34e2522
Omar <oomarusman@gmail.com>
2019-05-25 16:07:54 +0800

    Re-arrange namespaces.

--- src/Schema/Directives/RulesDirective.php
+++ src/Schema/Directives/RulesDirective.php
@@ -5,8 +5,8 @@ namespace Nuwave\Lighthouse\Schema\Directives;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 use Nuwave\Lighthouse\Support\Contracts\ArgValidationDirective;
-use Nuwave\Lighthouse\Support\Contracts\HasArgumentPath as HasArgumentPathContract;
 use Nuwave\Lighthouse\Support\Traits\HasArgumentPath as HasArgumentPathTrait;
+use Nuwave\Lighthouse\Support\Contracts\HasArgumentPath as HasArgumentPathContract;
 
 class RulesDirective extends BaseDirective implements ArgValidationDirective, HasArgumentPathContract
 {
