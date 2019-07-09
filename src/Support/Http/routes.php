08270c94a91985ebdf9bc1703b46b6fab9738158
Juuso Piikkilä <juuso@meiko.fi>
2019-07-09 09:45:36 +0300

    Add support for Lumen routing

--- src/Support/Http/routes.php
+++ src/Support/Http/routes.php
@@ -1,10 +1,16 @@
 <?php
 
+use Illuminate\Support\Str;
+
 if ($routeConfig = config('lighthouse.route')) {
-    /** @var \Illuminate\Contracts\Routing\Registrar $router */
+    /** @var \Illuminate\Contracts\Routing\Registrar|\Laravel\Lumen\Routing\Router $router */
     $router = app('router');
 
-    $router->match(
+    $method = Str::contains($this->app->version(), 'Lumen')
+        ? 'addRoute'
+        : 'match';
+
+    $router->$method(
         ['GET', 'POST'],
         $routeConfig['uri'] ?? 'graphql',
         [
