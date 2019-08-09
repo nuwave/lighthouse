fba8b2de85bc43f4f784fc36f8325aa2a4eed46c
Benedikt Franke <benedikt@franke.tech>
2019-08-09 13:18:30 +0200

    Eager-load the subscription fields to ensure they are registered (#909)

---
+++ tests/Unit/Subscriptions/Broadcast/BroadcastTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Tests\Unit\Subscriptions\Broadcast;
+
+use Tests\DBTestCase;
+use Mockery\MockInterface;
+use Tests\Utils\Models\Task;
+use Nuwave\Lighthouse\Execution\Utils\Subscription;
+use Nuwave\Lighthouse\Subscriptions\SubscriptionBroadcaster;
+use Nuwave\Lighthouse\Subscriptions\SubscriptionServiceProvider;
+
+class BroadcastTest extends DBTestCase
+{
+    protected $schema = '
+    type Task {
+        id: ID!
+        name: String!
+    }
+    
+    type Query {
+        task(id: Int @eq): Task @first
+    }
+    
+    type Mutation {
+        updateTask(id: Int! @eq, name: String!): Task @update @broadcast(subscription: "taskUpdated")
+    }
+    
+    type Subscription {
+        taskUpdated: Task
+    }
+    ';
+
+    protected function getPackageProviders($app)
+    {
+        return array_merge(
+            parent::getPackageProviders($app),
+            [SubscriptionServiceProvider::class]
+        );
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        factory(Task::class, 1)->create();
+    }
+
+    /**
+     * @return MockInterface
+     */
+    public function withMockedBroadcasts(): MockInterface
+    {
+        $broadcast = \Mockery::mock(SubscriptionBroadcaster::class);
+        $this->app->instance(SubscriptionBroadcaster::class, $broadcast);
+
+        return $broadcast;
+    }
+
+    /**
+     * @test
+     */
+    public function itBroadcastsFromPhp(): void
+    {
+        // Assert
+        $broadcast = $this->withMockedBroadcasts();
+        $broadcast->shouldReceive('broadcast')->once();
+
+        // Subscribe to event
+        $this->postGraphQL([
+            'query' => '
+                subscription UserUpdated {
+                    taskUpdated {
+                        name
+                    }
+                }
+            ',
+        ]);
+
+        // Broadcast
+        Subscription::broadcast('taskUpdated', []);
+    }
+
+    /**
+     * @test
+     */
+    public function itBroadcastsFromSchema(): void
+    {
+        // Assert
+        $broadcast = $this->withMockedBroadcasts();
+        $broadcast->shouldReceive('broadcast')->once();
+
+        // Subscribe to event
+        $this->postGraphQL([
+            'query' => '
+                subscription TaskUpdated {
+                    taskUpdated {
+                        name
+                    }
+                }
+            ',
+        ]);
+
+        // Broadcast
+        $this->postGraphQL([
+            'query' => '
+                mutation {
+                    updateTask(id: 1, name: "New name") {
+                        name
+                    }
+                }
+            ',
+        ]);
+    }
+}
