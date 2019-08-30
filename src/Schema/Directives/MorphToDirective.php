75edd4a855e2290f1c1ccc22e2dd282c2fb9fef0
Christopher Moore <chris@nuwavecommerce.com>
2019-08-30 08:39:59 +0000

    Apply fixes from StyleCI

--- src/Schema/Directives/MorphToDirective.php
+++ src/Schema/Directives/MorphToDirective.php
@@ -2,8 +2,8 @@
 
 namespace Nuwave\Lighthouse\Schema\Directives;
 
-use Nuwave\Lighthouse\Support\Contracts\DefinedDirective;
 use Nuwave\Lighthouse\Support\Contracts\FieldResolver;
+use Nuwave\Lighthouse\Support\Contracts\DefinedDirective;
 
 class MorphToDirective extends RelationDirective implements FieldResolver, DefinedDirective
 {
@@ -24,7 +24,7 @@ class MorphToDirective extends RelationDirective implements FieldResolver, Defin
      */
     public static function definition()
     {
-        return /** @lang GraphQL */ <<<SDL
+        return /* @lang GraphQL */ <<<'SDL'
 """
 Corresponds to [Eloquent's MorphTo-Relationship](https://laravel.com/docs/5.8/eloquent-relationships#one-to-one-polymorphic-relations).
 """
@@ -41,6 +41,5 @@ directive @morphTo(
   scopes: [String!]
 ) on FIELD_DEFINITION
 SDL;
-
     }
 }
