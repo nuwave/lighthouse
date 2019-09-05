428c62e714b6c085041dcd3a5e62cbe975ac7a97
lorado <eugen.sacharow@gmx.de>
2019-09-05 15:13:22 +0200

    revert changes on arraySubsetAsserts

--- tests/Integration/WhereConstraints/WhereConstraintsDirectiveTest.php
+++ tests/Integration/WhereConstraints/WhereConstraintsDirectiveTest.php
@@ -4,14 +4,11 @@ namespace Tests\Integration\WhereConstraints;
 
 use Tests\DBTestCase;
 use Tests\Utils\Models\User;
-use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
 use Nuwave\Lighthouse\WhereConstraints\WhereConstraintsDirective;
 use Nuwave\Lighthouse\WhereConstraints\WhereConstraintsServiceProvider;
 
 class WhereConstraintsDirectiveTest extends DBTestCase
 {
-    use ArraySubsetAsserts;
-
     protected $schema = '
     type User {
         id: ID!
