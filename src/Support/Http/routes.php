135bc64544e98b9eeee004d7fb902d514260596e
Danijel Dedic <dedic.d@gmail.com>
2019-09-05 16:09:54 +0200

    Wrap lighthouse.route with Group so we can use route specific config (like domain)

--- src/Support/Http/routes.php
+++ src/Support/Http/routes.php
@@ -6,18 +6,21 @@ if ($routeConfig = config('lighthouse.route')) {
     /** @var \Illuminate\Contracts\Routing\Registrar $router */
     $router = app('router');
 
-    $method = 'addRoute';
-    if (Str::startsWith(app()->version(), '5.5.')) {
-        $method = 'match';
-    }
+    $router->group(config('lighthouse.route', []), function () use ($router, $routeConfig): void {
 
-    $router->$method(
-        ['GET', 'POST'],
-        $routeConfig['uri'] ?? 'graphql',
-        [
-            'as' => $routeConfig['name'] ?? 'graphql',
-            'uses' => \Nuwave\Lighthouse\Support\Http\Controllers\GraphQLController::class.'@query',
-            'middleware' => $routeConfig['middleware'],
-        ]
-    );
+        $method = 'addRoute';
+        if (Str::startsWith(app()->version(), '5.5.')) {
+            $method = 'match';
+        }
+
+        $router->$method(
+            ['GET', 'POST'],
+            $routeConfig['uri'] ?? 'graphql',
+            [
+                'as' => $routeConfig['name'] ?? 'graphql',
+                'uses' => \Nuwave\Lighthouse\Support\Http\Controllers\GraphQLController::class.'@query',
+                'middleware' => $routeConfig['middleware'],
+            ]
+        );
+    });
 }
