2800caccbbce3167cfed3214aabb6ee98f7cd884
Benedikt Franke <benedikt@franke.tech>
2019-09-07 15:35:42 +0200

    format

--- src/Support/Http/routes.php
+++ src/Support/Http/routes.php
@@ -6,20 +6,23 @@ if ($routeConfig = config('lighthouse.route')) {
     /** @var \Illuminate\Contracts\Routing\Registrar $router */
     $router = app('router');
 
-    $router->group(config('lighthouse.route', []), function () use ($router, $routeConfig): void {
-        $method = 'addRoute';
-        if (Str::startsWith(app()->version(), '5.5.')) {
-            $method = 'match';
-        }
+    $router->group(
+        config('lighthouse.route', []),
+        function () use ($router, $routeConfig): void {
+            $method = 'addRoute';
+            if (Str::startsWith(app()->version(), '5.5.')) {
+                $method = 'match';
+            }
 
-        $router->$method(
-            ['GET', 'POST'],
-            $routeConfig['uri'] ?? 'graphql',
-            [
-                'as' => $routeConfig['name'] ?? 'graphql',
-                'uses' => \Nuwave\Lighthouse\Support\Http\Controllers\GraphQLController::class.'@query',
-                'middleware' => $routeConfig['middleware'],
-            ]
-        );
-    });
+            $router->$method(
+                ['GET', 'POST'],
+                $routeConfig['uri'] ?? 'graphql',
+                [
+                    'as' => $routeConfig['name'] ?? 'graphql',
+                    'uses' => \Nuwave\Lighthouse\Support\Http\Controllers\GraphQLController::class.'@query',
+                    'middleware' => $routeConfig['middleware'],
+                ]
+            );
+        }
+    );
 }
