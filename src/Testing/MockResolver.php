176818179d6edca9946bac67609944d3ea00e151
Benedikt Franke <benedikt@franke.tech>
2019-10-02 12:40:09 +0000

    Apply fixes from StyleCI

--- src/Testing/MockResolver.php
+++ src/Testing/MockResolver.php
@@ -4,7 +4,7 @@ namespace Nuwave\Lighthouse\Testing;
 
 /**
  * TODO remove in favour of ->getMockBuilder(\stdClass::class)->addMethods(['__invoke'])
- * once we no longer support PHPUnit 7
+ * once we no longer support PHPUnit 7.
  */
 class MockResolver
 {
