b846137a6d93041d9f5c7d6b02c1c78464100c63
Benedikt Franke <benedikt@franke.tech>
2019-10-10 11:24:43 +0000

    Apply fixes from StyleCI

--- src/Execution/Arguments/TypedArgs.php
+++ src/Execution/Arguments/TypedArgs.php
@@ -2,11 +2,11 @@
 
 namespace Nuwave\Lighthouse\Execution\Arguments;
 
-use GraphQL\Language\AST\InputValueDefinitionNode;
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Schema\AST\ASTHelper;
 use Nuwave\Lighthouse\Schema\AST\ASTBuilder;
 use GraphQL\Language\AST\FieldDefinitionNode;
+use GraphQL\Language\AST\InputValueDefinitionNode;
 use GraphQL\Language\AST\InputObjectTypeDefinitionNode;
 
 class TypedArgs
