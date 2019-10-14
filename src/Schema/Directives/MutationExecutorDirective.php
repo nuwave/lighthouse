174464ff90ddbd2f31a38dc557c35108bca66298
spawnia <benedikt@franke.tech>
2019-10-14 20:08:47 +0200

    Fix syntax

--- src/Schema/Directives/MutationExecutorDirective.php
+++ src/Schema/Directives/MutationExecutorDirective.php
@@ -51,8 +51,9 @@ abstract class MutationExecutorDirective extends BaseDirective implements FieldR
     {
         return $fieldValue->setResolver(
             function ($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): Model {
+                $modelClass = $this->getModelClass();
                 /** @var \Illuminate\Database\Eloquent\Model $model */
-                $model = new ($this->getModelClass());
+                $model = new $modelClass;
 
                 $executeMutation = function () use ($model, $args): Model {
                     return $this
