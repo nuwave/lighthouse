df51af9a76f6a7117a4a025f56fae756e5ecce8c
Benedikt Franke <benedikt.franke@mll.com>
2019-10-16 13:49:45 +0200

    Reproduce https://github.com/nuwave/lighthouse/issues/1004

---
+++ tests/Integration/SchemaCachingTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Integration;
+
+use Illuminate\Cache\CacheManager;
+use Illuminate\Cache\Repository;
+use Nuwave\Lighthouse\Schema\AST\ASTBuilder;
+use Tests\SerializingArrayStore;
+use Tests\TestCase;
+use Tests\Utils\Models\Comment;
+
+class SchemaCachingTest extends TestCase
+{
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        /** @var \Illuminate\Contracts\Config\Repository $config */
+        $config = $app['config'];
+
+        $config->set('lighthouse.cache.enable', true);
+
+        /** @var \Illuminate\Cache\CacheManager $cache */
+        $cache = $app->make(CacheManager::class);
+        $cache->extend('serializing-array', function(){
+            return new Repository(
+                new SerializingArrayStore()
+            );
+        });
+        $config->set('cache.stores.array.driver', 'serializing-array');
+    }
+
+    public function testSchemaCachingWithUnionType(): void
+    {
+        $this->schema = /** @lang GraphQL */ '
+        type Query {
+            foo: Foo @mock
+        }
+        
+        union Foo = Comment | Color
+        
+        type Comment {
+            bar: ID
+        }
+        
+        type Color {
+            id: ID
+        }
+        ';
+        $this->cacheSchema();
+
+        $this->mockResolver(function() {
+            return new Comment([
+                'bar' => 'bar'
+            ]);
+            $comment->id = 'bar';
+            return $comment;
+        });
+
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo {
+                ... on Comment {
+                    bar
+                }
+            }
+        }
+        ')->assertExactJson([
+            'data' => [
+                'foo' => [
+                    'bar' => 'bar'
+                ]
+            ]
+        ]);
+    }
+
+    protected function cacheSchema(): void
+    {
+        /** @var \Nuwave\Lighthouse\Schema\AST\ASTBuilder $astBuilder */
+        $astBuilder = app(ASTBuilder::class);
+        $astBuilder->documentAST();
+        $this->app->forgetInstance(ASTBuilder::class);
+    }
+}
