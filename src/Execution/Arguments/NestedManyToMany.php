ecd7c3d24ab709d0ecb468ccf4a1e2855a5c5263
spawnia <benedikt@franke.tech>
2019-10-27 21:31:19 +0100

    Extract duplicate code

--- src/Execution/Arguments/NestedManyToMany.php
+++ src/Execution/Arguments/NestedManyToMany.php
@@ -27,30 +27,7 @@ class NestedManyToMany implements ArgumentResolver
             );
         }
 
-        /* @var \Nuwave\Lighthouse\Execution\Arguments\Argument|null $create */
-        if (isset($args->arguments['create'])) {
-            $saveModel = new ArgResolver(new SaveModel($relation));
-
-            foreach ($args->arguments['create']->value as $childArgs) {
-                $saveModel($relation->make(), $childArgs);
-            }
-        }
-
-        if (isset($args->arguments['update'])) {
-            $updateModel = new ArgResolver(new UpdateModel(new SaveModel($relation)));
-
-            foreach ($args->arguments['update']->value as $childArgs) {
-                $updateModel($relation->make(), $childArgs);
-            }
-        }
-
-        if (isset($args->arguments['upsert'])) {
-            $upsertModel = new ArgResolver(new UpsertModel(new SaveModel($relation)));
-
-            foreach ($args->arguments['upsert']->value as $childArgs) {
-                $upsertModel($relation->make(), $childArgs);
-            }
-        }
+        NestedOneToMany::createUpdateUpsert($args, $relation);
 
         if (isset($args->arguments['delete'])) {
             $ids = $args->arguments['delete']->toPlain();
--- src/Execution/Arguments/NestedOneToMany.php
+++ src/Execution/Arguments/NestedOneToMany.php
@@ -2,6 +2,7 @@
 
 namespace Nuwave\Lighthouse\Execution\Arguments;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Nuwave\Lighthouse\Execution\ArgumentResolver;
 
 class NestedOneToMany implements ArgumentResolver
@@ -21,6 +22,21 @@ class NestedOneToMany implements ArgumentResolver
         /** @var \Illuminate\Database\Eloquent\Relations\HasMany|\Illuminate\Database\Eloquent\Relations\MorphMany $relation */
         $relation = $model->{$this->relationName}();
 
+        static::createUpdateUpsert($args, $relation);
+
+        if (isset($args->arguments['delete'])) {
+            $relation->getRelated()::destroy(
+                $args->arguments['delete']->toPlain()
+            );
+        }
+    }
+
+    /**
+     * @param  \Nuwave\Lighthouse\Execution\Arguments\ArgumentSet  $args
+     * @param  \Illuminate\Database\Eloquent\Relations\Relation  $relation
+     */
+    public static function createUpdateUpsert(ArgumentSet $args, Relation $relation): void
+    {
         if (isset($args->arguments['create'])) {
             $saveModel = new ArgResolver(new SaveModel($relation));
 
@@ -43,12 +59,6 @@ class NestedOneToMany implements ArgumentResolver
             foreach ($args->arguments['upsert']->value as $childArgs) {
                 $upsertModel($relation->make(), $childArgs);
             }
-        }
-
-        if (isset($args->arguments['delete'])) {
-            $relation->getRelated()::destroy(
-                $args->arguments['delete']->toPlain()
-            );
         }
     }
 }
