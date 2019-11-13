3630721332e98d139532d016b595a765ab0d253a
Benedikt Franke <benedikt.franke@mll.com>
2019-11-13 19:10:47 +0100

    Add CHANGELOG

--- src/Schema/ResolverProvider.php
+++ src/Schema/ResolverProvider.php
@@ -46,7 +46,7 @@ class ResolverProvider implements ProvidesResolver
                 $proposedResolverClass = ucfirst($fieldName);
 
                 throw new DefinitionException(<<<MESSAGE
-Could not locate a default resolver for the {$parent}: {$fieldName}.
+Could not locate a field resolver for the {$parent}: {$fieldName}.
 
 Either add a resolver directive such as @all, @find or @create or add
 a resolver class through:
