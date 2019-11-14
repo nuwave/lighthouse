09a0df93a9b4d409d5b074ce0d42a5cffbfe5979
Fabian Torres <fa.iverson@gmail.com>
2019-11-14 04:15:43 -0300

    Add `injectArgs` option to `@can` directive to pass along client defined   arguments to the policy check (#1043)

--- docs/master/api-reference/directives.md
+++ docs/master/api-reference/directives.md
@@ -459,6 +459,9 @@ class PostPolicy
 ```graphql
 """
 Check a Laravel Policy to ensure the current user is authorized to access a field.
+
+When `injectArgs` and `args` are used together, the client given
+arguments will be passed before the static args.
 """
 directive @can(
   """
@@ -471,18 +474,26 @@ directive @can(
   instance against which the permissions should be checked.
   """
   find: String
+
+  """
+  Pass along the client given input data as arguments to `Gate::check`. 
+  """
+  injectArgs: Boolean = false
+
+  """
+  Statically defined arguments that are passed to `Gate::check`.
   
+  You may pass pass arbitrary GraphQL literals,
+  e.g.: [1, 2, 3] or { foo: "bar" }
   """
-  Additional arguments that are passed to `Gate::check`. 
-  """
-  args: [String!]
+  args: Mixed
 ) on FIELD_DEFINITION
 ```
 
 ### Examples
 
 In `find` parameter you may specify an input argument which is used to find a specific model
-instance by primary key against which the permissions should be checked.
+instance by primary key against which the permissions should be checked:
 
 ```graphql
 type Query {
@@ -500,7 +511,7 @@ class PostPolicy
 }
 ```
 
-It also works with soft deleted models in combination with `@softDeletes` directive.
+It also works with soft deleted models in combination with `@softDeletes` directive:
 
 ```graphql
 type Query {
@@ -518,7 +529,7 @@ type Mutation {
 }
 ```
 
-You can pass additional arguments to the policy checks by specifying them as `args`.
+You can pass additional arguments to the policy checks by specifying them as `args`:
 
 ```graphql
 type Mutation {
@@ -526,6 +537,18 @@ type Mutation {
         @can(ability: "create", args: ["FROM_GRAPHQL"])
 }
 ```
+
+You can pass along the client given input data as arguments to the policy checks
+with the `injectArgs` argument:
+
+```graphql
+type Mutation {
+    createPost(input: PostInput): Post
+        @can(ability: "create", injectArgs: "true")
+}
+```
+
+Now you will have access to `PostInput` values in the policy. 
 
 Starting from Laravel 5.7, [authorization of guest users](https://laravel.com/docs/authorization#guest-users) is supported.
 Because of this, Lighthouse does **not** validate that the user is authenticated before passing it along to the policy.
--- src/Schema/Directives/CanDirective.php
+++ src/Schema/Directives/CanDirective.php
@@ -7,6 +7,7 @@ use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Database\Eloquent\Model;
 use Nuwave\Lighthouse\Exceptions\AuthorizationException;
+use Nuwave\Lighthouse\Execution\Arguments\ArgumentSet;
 use Nuwave\Lighthouse\Schema\Values\FieldValue;
 use Nuwave\Lighthouse\SoftDeletes\TrashedDirective;
 use Nuwave\Lighthouse\Support\Contracts\DefinedDirective;
@@ -46,6 +47,9 @@ class CanDirective extends BaseDirective implements FieldMiddleware, DefinedDire
         return /* @lang GraphQL */ <<<'SDL'
 """
 Check a Laravel Policy to ensure the current user is authorized to access a field.
+
+When `injectArgs` and `args` are used together, the client given
+arguments will be passed before the static args.
 """
 directive @can(
   """
@@ -58,11 +62,19 @@ directive @can(
   instance against which the permissions should be checked.
   """
   find: String
+
+  """
+  Pass along the client given input data as arguments to `Gate::check`. 
+  """
+  injectArgs: Boolean = false
+
+  """
+  Statically defined arguments that are passed to `Gate::check`.
   
+  You may pass pass arbitrary GraphQL literals,
+  e.g.: [1, 2, 3] or { foo: "bar" }
   """
-  Additional arguments that are passed to `Gate::check`. 
-  """
-  args: [String!]
+  args: Mixed
 ) on FIELD_DEFINITION
 SDL;
     }
@@ -81,58 +93,66 @@ SDL;
         return $next(
             $fieldValue->setResolver(
                 function ($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) use ($previousResolver) {
-                    if ($find = $this->directiveArgValue('find')) {
-                        $modelOrModels = $resolveInfo
-                            ->argumentSet
-                            ->enhanceBuilder(
-                                $this->getModelClass()::query(),
-                                [],
-                                function (Directive $directive): bool {
-                                    return $directive instanceof TrashedDirective;
-                                }
-                            )
-                            ->findOrFail($args[$find]);
+                    $gate = $this->gate->forUser($context->user());
+                    $ability = $this->directiveArgValue('ability');
+                    $checkArguments = $this->buildCheckArguments($args);
 
-                        if ($modelOrModels instanceof Model) {
-                            $modelOrModels = [$modelOrModels];
-                        }
-
-                        /** @var \Illuminate\Database\Eloquent\Model $model */
-                        foreach ($modelOrModels as $model) {
-                            $this->authorize($context->user(), $model);
-                        }
-                    } else {
-                        $this->authorize($context->user(), $this->getModelClass());
+                    foreach ($this->modelsToCheck($resolveInfo->argumentSet, $args) as $model) {
+                        $this->authorize($gate, $ability, $model, $checkArguments);
                     }
 
-                    return call_user_func_array($previousResolver, func_get_args());
+                    return $previousResolver($root, $args, $context, $resolveInfo);
                 }
             )
         );
     }
 
     /**
-     * @param  \Illuminate\Contracts\Auth\Authenticatable|null  $user
+     * @param  \Nuwave\Lighthouse\Execution\Arguments\ArgumentSet  $argumentSet
+     * @param  array  $args
+     * @return iterable<Model|string>
+     *
+     * @throws \Nuwave\Lighthouse\Exceptions\DefinitionException
+     */
+    protected function modelsToCheck(ArgumentSet $argumentSet, array $args): iterable
+    {
+        if ($find = $this->directiveArgValue('find')) {
+            $modelOrModels = $argumentSet
+                ->enhanceBuilder(
+                    $this->getModelClass()::query(),
+                    [],
+                    function (Directive $directive): bool {
+                        return $directive instanceof TrashedDirective;
+                    }
+                )
+                ->findOrFail($args[$find]);
+
+            if ($modelOrModels instanceof Model) {
+                $modelOrModels = [$modelOrModels];
+            }
+
+            return $modelOrModels;
+        }
+
+        return [$this->getModelClass()];
+    }
+
+    /**
+     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
+     * @param  string|string[]  $ability
      * @param  string|\Illuminate\Database\Eloquent\Model  $model
+     * @param  array  $arguments
      * @return void
      *
      * @throws \Nuwave\Lighthouse\Exceptions\AuthorizationException
      */
-    protected function authorize($user, $model): void
+    protected function authorize(Gate $gate, $ability, $model, array $arguments): void
     {
         // The signature of the second argument `$arguments` of `Gate::check`
         // should be [modelClassName, additionalArg, additionalArg...]
-        $arguments = $this->getAdditionalArguments();
         array_unshift($arguments, $model);
 
-        $can = $this->gate
-            ->forUser($user)
-            ->check(
-                $this->directiveArgValue('ability'),
-                $arguments
-            );
-
-        if (! $can) {
+        if (! $gate->check($ability, $arguments)) {
             throw new AuthorizationException(
                 "You are not authorized to access {$this->definitionNode->name->value}"
             );
@@ -142,10 +162,22 @@ SDL;
     /**
      * Additional arguments that are passed to `Gate::check`.
      *
+     * @param  array  $args
      * @return mixed[]
      */
-    protected function getAdditionalArguments(): array
+    protected function buildCheckArguments(array $args): array
     {
-        return (array) $this->directiveArgValue('args');
+        $checkArguments = [];
+
+        // The injected args come before the static args
+        if ($this->directiveArgValue('injectArgs')) {
+            $checkArguments [] = $args;
+        }
+
+        if ($this->directiveHasArgument('args')) {
+            $checkArguments [] = $this->directiveArgValue('args');
+        }
+
+        return $checkArguments;
     }
 }
