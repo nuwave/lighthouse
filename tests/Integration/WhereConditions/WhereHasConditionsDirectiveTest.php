7dcbd06be65af09e8fcca707d9b40c96ea71717e
Yanik Pahl <ch3sc0s@gmail.com>
2020-01-14 14:18:59 +0100

    removed redundant tests and added 2 new tests

--- tests/Integration/WhereConditions/WhereHasConditionsDirectiveTest.php
+++ tests/Integration/WhereConditions/WhereHasConditionsDirectiveTest.php
@@ -61,386 +61,21 @@ class WhereHasConditionsDirectiveTest extends DBTestCase
         );
     }
 
-    public function testDefaultsToWhereEqual(): void
+    public function testIgnoreEmptyConditions(): void
     {
-        factory(User::class, 2)->create();
+        factory(User::class)->create([
+            'company_id' => null,
+        ]);
 
         $this->graphQL(/** @lang GraphQL */ '
         {
             users(
-                hasCompany: {
-                    column: "id"
-                    value: 1
-                }
+                hasCompany: {}
             ) {
                 id
             }
         }
         ')->assertJsonCount(1, 'data.users');
-    }
-
-    public function testOverwritesTheOperator(): void
-    {
-        factory(User::class, 3)->create();
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            users(
-                hasCompany: {
-                    column: "id"
-                    operator: GT
-                    value: 1
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertJsonCount(2, 'data.users');
-    }
-
-    public function testOperatorIn(): void
-    {
-        factory(User::class, 5)->create();
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            users(
-                hasCompany: {
-                    column: "id",
-                    operator: IN
-                    value: [2, 5]
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertExactJson([
-            'data' => [
-                'users' => [
-                    [
-                        'id' => '2',
-                    ],
-                    [
-                        'id' => '5',
-                    ],
-                ],
-            ],
-        ]);
-    }
-
-    public function testOperatorIsNull(): void
-    {
-        factory(Post::class)->create([
-            'body' => null,
-        ]);
-        factory(Post::class)->create([
-            'body' => 'foobar',
-        ]);
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            users(
-                hasPost: {
-                    column: "body",
-                    operator: IS_NULL
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertExactJson([
-            'data' => [
-                'users' => [
-                    [
-                        'id' => '1',
-                    ],
-                ],
-            ],
-        ]);
-    }
-
-    public function testOperatorNotNull(): void
-    {
-        factory(Post::class)->create([
-            'body' => null,
-        ]);
-        factory(Post::class)->create([
-            'body' => 'foobar',
-        ]);
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            users(
-                hasPost: {
-                    column: "body",
-                    operator: IS_NOT_NULL
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertExactJson([
-            'data' => [
-                'users' => [
-                    [
-                        'id' => '2',
-                    ],
-                ],
-            ],
-        ]);
-    }
-
-    public function testOperatorNotBetween(): void
-    {
-        factory(User::class, 5)->create();
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            users(
-                hasCompany: {
-                    column: "id",
-                    operator: NOT_BETWEEN
-                    value: [2, 4]
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertExactJson([
-            'data' => [
-                'users' => [
-                    [
-                        'id' => '1',
-                    ],
-                    [
-                        'id' => '5',
-                    ],
-                ],
-            ],
-        ]);
-    }
-
-    public function testAddsNestedAnd(): void
-    {
-        factory(User::class, 3)->create();
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            users(
-                hasCompany: {
-                    AND: [
-                        {
-                            column: "id"
-                            operator: GT
-                            value: 1
-                        }
-                        {
-                            column: "id"
-                            operator: LT
-                            value: 3
-                        }
-                    ]
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertJsonCount(1, 'data.users');
-    }
-
-    public function testAddsNestedOr(): void
-    {
-        factory(User::class, 5)->create();
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            users(
-                hasCompany: {
-                    OR: [
-                        {
-                            column: "id"
-                            value: 1
-                        }
-                        {
-                            column: "id"
-                            value: 3
-                        }
-                        {
-                            OR: [
-                                {
-                                    column: "id"
-                                    value: 5
-                                }
-                            ]
-
-                        }
-                    ]
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertExactJson([
-            'data' => [
-                'users' => [
-                    [
-                        'id' => '1',
-                    ],
-                    [
-                        'id' => '3',
-                    ],
-                    [
-                        'id' => '5',
-                    ],
-                ],
-            ],
-        ]);
-    }
-
-    public function testRejectsInvalidColumnName(): void
-    {
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            users(
-                hasCompany: {
-                    AND: [
-                        {
-                            column: "Robert\'); DROP TABLE Students;--"
-                            value: "https://xkcd.com/327/"
-                        }
-                    ]
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertJsonFragment([
-            'message' => WhereHasConditionsDirective::invalidColumnName("Robert'); DROP TABLE Students;--"),
-        ]);
-    }
-
-    public function testQueriesEmptyStrings(): void
-    {
-        factory(User::class, 3)->create();
-
-        $userNamedEmptyString = factory(User::class)->create([
-            'name' => '',
-        ]);
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            companies(
-                hasUser: {
-                    column: "name"
-                    value: ""
-                }
-            ) {
-                id
-                name
-            }
-        }
-        ')->assertJson([
-            'data' => [
-                'companies' => [
-                    [
-                        'id' => $userNamedEmptyString->company_id,
-                    ],
-                ],
-            ],
-        ]);
-    }
-
-    public function testCanQueryForNull(): void
-    {
-        factory(User::class, 3)->create();
-
-        $userNamedNull = factory(User::class)->create([
-            'name' => null,
-        ]);
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            companies(
-                hasUser: {
-                    column: "name"
-                    value: null
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertJson([
-            'data' => [
-                'companies' => [
-                    [
-                        'id' => $userNamedNull->company_id,
-                    ],
-                ],
-            ],
-        ]);
-    }
-
-    public function testRequiresAValueForAColumn(): void
-    {
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            users(
-                hasPost: {
-                    column: "no_value"
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertJsonFragment([
-            'message' => SQLOperator::missingValueForColumn('no_value'),
-        ]);
-    }
-
-    public function testOnlyAllowsWhitelistedColumns(): void
-    {
-        factory(User::class)->create();
-
-        $this->graphQL(/* @lang GraphQL */ '
-        {
-            whitelistedColumns(
-                hasCompany: {
-                    column: ID
-                    value: 1
-                }
-            ) {
-                id
-            }
-        }
-        ')->assertJson([
-            'data' => [
-                'whitelistedColumns' => [
-                    [
-                        'id' => 1,
-                    ],
-                ],
-            ],
-        ]);
-
-        $expectedEnumName = 'WhitelistedColumnsHasCompanyColumn';
-        $enum = $this->introspectType($expectedEnumName);
-
-        $this->assertArraySubset(
-            [
-                'kind' => 'ENUM',
-                'name' => $expectedEnumName,
-                'description' => 'Allowed column names for the `hasCompany` argument on the query `whitelistedColumns`.',
-                'enumValues' => [
-                    [
-                        'name' => 'ID',
-                    ],
-                    [
-                        'name' => 'CAMEL_CASE',
-                    ],
-                ],
-            ],
-            $enum
-        );
     }
 
     public function testWithoutRelationName(): void
@@ -463,6 +98,43 @@ class WhereHasConditionsDirectiveTest extends DBTestCase
                 'withoutRelation' => [
                     [
                         'id' => 1,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function testOperatorOr(): void
+    {
+        factory(User::class, 5)->create();
+
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            users(
+                hasCompany: {
+                    OR: [
+                        {
+                            column: "id"
+                            value: 1
+                        }
+                        {
+                            column: "id"
+                            value: 3
+                        }
+                    ]
+                }
+            ) {
+                id
+            }
+        }
+        ')->assertExactJson([
+            'data' => [
+                'users' => [
+                    [
+                        'id' => '1',
+                    ],
+                    [
+                        'id' => '3',
                     ],
                 ],
             ],
