41eea51319778e7b144cecca4301a964a248edae
Benedikt Franke <benedikt.franke@mll.com>
2020-01-22 15:20:10 +0100

    Offer alternative

--- src/Schema/Directives/RulesDirective.php
+++ src/Schema/Directives/RulesDirective.php
@@ -31,7 +31,8 @@ directive @rules(
   This can either be a reference to [Laravel's built-in validation rules](https://laravel.com/docs/validation#available-validation-rules),
   or the fully qualified class name of a custom validation rule.
 
-  Rules that mutate the incoming arguments, such as `exclude_if`, are not supported.
+  Rules that mutate the incoming arguments, such as `exclude_if`, are not supported
+  by Lighthouse. Use ArgTransformerDirectives or FieldMiddlewareDirectives instead.
   """
   apply: [String!]!
 
