5ab6155cf7439b70d5740e269d1f3fd8b0540fa0
Benedikt Franke <benedikt.franke@mll.com>
2020-02-17 09:27:09 +0100

    Ensure toResponse is not called in Pipeline

---
+++ tests/Support/PipelineTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\Support;
+
+use Illuminate\Contracts\Support\Responsable;
+use Tests\TestCase;
+
+class PipelineTest extends TestCase
+{
+    public function testDoesNotCallToResponse(): void
+    {
+        $this->mockResolver(new class implements Responsable {
+            public $bar = 42;
+
+            public function toResponse($request)
+            {
+                throw new \Exception('Must not be called when returning this from a resolver.');
+            }
+        });
+
+        $this->schema = /** @lang GraphQL */ '
+        type Query {
+            foo: Foo @mock
+        }
+
+        type Foo {
+            bar: Int
+        }
+        ';
+
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo {
+                bar
+            }
+        }
+        ')->assertExactJson([
+            'data' => [
+                'foo' => [
+                    'bar' => 42
+                ]
+            ]
+        ]);
+    }
+}
