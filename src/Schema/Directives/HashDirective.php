49da875128a53da719c716c03951f8736048ec9d
Chrissi2812 <cf@pixel-kraft.de>
2020-02-19 13:15:51 +0100

    add hash directive

---
+++ src/Schema/Directives/HashDirective.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Nuwave\Lighthouse\Schema\Directives;
+
+use Illuminate\Support\Facades\Hash;
+use Nuwave\Lighthouse\Support\Contracts\ArgTransformerDirective;
+use Nuwave\Lighthouse\Support\Contracts\DefinedDirective;
+
+class HashDirective extends BaseDirective implements ArgTransformerDirective, DefinedDirective
+{
+    public static function definition(): string
+    {
+        return /** @lang GraphQL */ <<<'SDL'
+"""
+Run the `Hash::make` function on the argument it is defined on.
+"""
+directive @hash on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION
+SDL;
+    }
+
+    /**
+     * Run Laravel's Hash::make on the argument.
+     *
+     * Useful for hashing passwords before inserting them into the database.
+     * Uses the hashing driver defined in config/hashing.php
+     *
+     * @param  string  $argumentValue
+     * @return string
+     */
+    public function transform($argumentValue): string
+    {
+        return Hash::make($argumentValue);
+    }
+}
