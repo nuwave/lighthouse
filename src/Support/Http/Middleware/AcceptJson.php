330183c10ef3dd21c56e7d0ca3de7a495bcf68fb
Max <49632507+maxm86545@users.noreply.github.com>
2020-03-03 17:35:46 +0300

    Correct work with header "Content-Type: text/plain;charset=UTF-8"
    Correct work with header "Content-Type: text/plain;charset=UTF-8": https://github.com/apollographql/apollo-link/issues/275

--- src/Support/Http/Middleware/AcceptJson.php
+++ src/Support/Http/Middleware/AcceptJson.php
@@ -25,7 +25,7 @@ class AcceptJson
      */
     public function handle(Request $request, Closure $next)
     {
-        $request->headers->set('Accept', 'application/json');
+        $request->headers->set('Content-Type', 'application/json');
 
         return $next($request);
     }
