6adf4cf4ebf96d55d8887fea937b547555f6be19
spawnia <benedikt@franke.tech>
2020-04-19 15:22:43 +0200

    Report errors that are not client safe

--- src/Execution/ReportingErrorHandler.php
+++ src/Execution/ReportingErrorHandler.php
@@ -13,16 +13,16 @@ class ReportingErrorHandler implements ErrorHandler
 {
     public static function handle(Error $error, Closure $next): array
     {
-        $previous = $error->getPrevious();
-
-        // If the Error does not wrap another Error, it is related to a client error
-        // and shown in the error response anyway, we don't really need to report it
-        if ($previous) {
-            // TODO inject through constructor once handle is non-static
-            /** @var \Illuminate\Contracts\Debug\ExceptionHandler $reporter */
-            $reporter = app(ExceptionHandler::class);
-            $reporter->report($previous);
+        // Client-safe errors are assumed to be something that a client can handle
+        // or is expected to happen, e.g. wrong syntax, authentication or validation
+        if ($error->isClientSafe()) {
+            return $next($error);
         }
+
+        // TODO inject through constructor once handle is non-static
+        /** @var \Illuminate\Contracts\Debug\ExceptionHandler $reporter */
+        $reporter = app(ExceptionHandler::class);
+        $reporter->report($error->getPrevious());
 
         return $next($error);
     }
