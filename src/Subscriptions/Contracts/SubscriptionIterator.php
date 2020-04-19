b48270af8fe24bbfd2351d8e0796ebf924067bb3
spawnia <benedikt@franke.tech>
2020-04-19 18:59:09 +0200

    Clarify SubscriptionIterator.php docblock

--- src/Subscriptions/Contracts/SubscriptionIterator.php
+++ src/Subscriptions/Contracts/SubscriptionIterator.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Collection;
 interface SubscriptionIterator
 {
     /**
-     * Process subscribers.
+     * Process subscribers through the given callbacks.
      *
      * @param  \Illuminate\Support\Collection<\Nuwave\Lighthouse\Subscriptions\Subscriber>  $subscribers
      * The subscribers that receive the current subscription.
@@ -17,9 +17,9 @@ interface SubscriptionIterator
      * Receives each subscriber in the passed in collection.
      * function(\Nuwave\Lighthouse\Subscriptions\Subscriber $subscriber)
      *
-     * @param  \Closure|null  $onError
+     * @param  \Closure|null  $handleError
      * Is called when $handleSubscriber throws.
      * @return void
      */
-    public function process(Collection $subscribers, Closure $handleSubscriber, Closure $onError = null);
+    public function process(Collection $subscribers, Closure $handleSubscriber, Closure $handleError = null);
 }
