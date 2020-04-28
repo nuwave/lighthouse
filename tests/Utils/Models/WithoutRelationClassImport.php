4d1ea9ae44e7b903f75ffa6e4f012167534bbcf2
Benedikt Franke <benedikt@franke.tech>
2020-04-28 18:56:46 +0200

    Update WithoutRelationClassImport.php

--- tests/Utils/Models/WithoutRelationClassImport.php
+++ tests/Utils/Models/WithoutRelationClassImport.php
@@ -4,15 +4,9 @@ namespace Tests\Utils\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * @property int $id
- * @property string $name
- */
 class WithoutRelationClassImport extends Model
 {
-    protected $guarded = [];
-
-    public function users(): HasMany // @phpstan-ignore-line Missing the import on purpose
+    public function users(): HasMany // Missing the import on purpose
     {
         return $this->hasMany(User::class);
     }
