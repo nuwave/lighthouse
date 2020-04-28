e454a9f019dca3a1a4463f280eb8be52f14987d7
tjbp <tjbp@users.noreply.github.com>
2020-04-28 18:02:09 +0100

    Throw an exception if the return type declaration class for a relation does not exist (#1338)

--- tests/Unit/Execution/Arguments/ArgPartitionerTest.php
+++ tests/Unit/Execution/Arguments/ArgPartitionerTest.php
@@ -3,12 +3,14 @@
 namespace Tests\Unit\Execution\Arguments;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Nuwave\Lighthouse\Exceptions\DefinitionException;
 use Nuwave\Lighthouse\Execution\Arguments\ArgPartitioner;
 use Nuwave\Lighthouse\Execution\Arguments\Argument;
 use Nuwave\Lighthouse\Execution\Arguments\ArgumentSet;
 use Tests\TestCase;
 use Tests\Unit\Execution\Arguments\Fixtures\Nested;
 use Tests\Utils\Models\User;
+use Tests\Utils\Models\WithoutRelationClassImport;
 
 class ArgPartitionerTest extends TestCase
 {
@@ -60,6 +62,22 @@ class ArgPartitionerTest extends TestCase
         $this->assertSame(
             ['tasks' => $tasksRelation],
             $hasManyArgs->arguments
+        );
+    }
+
+    public function testPartitionArgsExceptionBadRelationType(): void
+    {
+        $argumentSet = new ArgumentSet();
+
+        $tasksRelation = new Argument();
+        $argumentSet->arguments['users'] = $tasksRelation;
+
+        $this->expectException(DefinitionException::class);
+
+        [$hasManyArgs, $regularArgs] = ArgPartitioner::relationMethods(
+            $argumentSet,
+            new WithoutRelationClassImport(),
+            HasMany::class
         );
     }
 }
