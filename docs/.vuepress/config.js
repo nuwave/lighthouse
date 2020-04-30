706a5bfa08867bcce2aad93b7724f9608dd8f403
spawnia <benedikt@franke.tech>
2020-04-30 21:59:19 +0200

    Upgrade vuepress and some config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,7 +24,6 @@ module.exports = {
         defaultTheme: 'light',
         logo: '/logo.svg',
         editLinks: true, //  "Edit this page" at the bottom of each page
-        lastUpdated: 'Last Updated', //  "Last Updated" at the bottom of each page
         repo: 'nuwave/lighthouse', //  Github repo
         docsDir: 'docs/', //  Github repo docs folder
         versions: {
