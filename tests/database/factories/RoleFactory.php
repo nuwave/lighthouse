f0a4eafa8738416913beaf990f2efc33d315a838
Benedikt Franke <benedikt.franke@mll.com>
2020-05-06 09:11:32 +0200

    Fix unique role name generation

--- tests/database/factories/RoleFactory.php
+++ tests/database/factories/RoleFactory.php
@@ -6,10 +6,8 @@ use Tests\Utils\Models\Role;
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(Role::class, function (Faker $faker): array {
-    $roles = ['role_1', 'role_2', 'role_3', 'role_4', 'role_5', 'role_6'];
-
     return [
-        'name' => $faker->unique()->randomElement($roles),
+        'name' => 'role_' . $faker->unique()->randomNumber(),
         'acl_id' => function () {
             return factory(ACL::class)->create()->getKey();
         },
