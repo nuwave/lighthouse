fc484a1af603e5904adce7d408615285f1b05e77
Konstantin Duzcek <konz.duczek@gmail.com>
2020-05-19 22:00:31 +0200

    [redis subscriptions] add event class for echo broadcasting

---
+++ src/Subscriptions/Events/EchoSubscriptionEvent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Nuwave\Lighthouse\Subscriptions\Events;
+
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class EchoSubscriptionEvent implements ShouldBroadcast
+{
+    /**
+     * @var string
+     */
+    public $channel;
+
+    /**
+     * @var mixed
+     */
+    public $data;
+
+    /**
+     * @param string $channel
+     * @param mixed $data
+     */
+    public function __construct(string $channel, $data)
+    {
+        $this->channel = $channel;
+        $this->data = $data;
+    }
+
+    /**
+     * @return PresenceChannel
+     */
+    public function broadcastOn()
+    {
+        return new PresenceChannel($this->channel);
+    }
+
+    /**
+     * Returns an event name.
+     * Allows the echo client to receive this event using .listen('.lighthouse.subscription', () => ...).
+     * @return string
+     */
+    public function broadcastAs()
+    {
+        return 'lighthouse.subscription';
+    }
+}
