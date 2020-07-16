c6d9ef626b7488bc751be9a7d030442a6c05270d
Thiery Laverdure <thiery@spacestud.io>
2020-07-16 07:45:24 -0400

    Update tests/Utils/Models/Activity.php
    Co-authored-by: Benedikt Franke <benedikt@franke.tech>

--- tests/Utils/Models/Activity.php
+++ tests/Utils/Models/Activity.php
@@ -12,6 +12,10 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
  * @property string $activity_type
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
+ *
+ * Relations
+ * @property-read \Tests\Utils\Models\Post|\Tests\Utils\Models\Task $content
+ * @property-read \Tests\Utils\Models\User $user
  */
 class Activity extends Model
 {
