8f0a1d00aa8fb788a1670f3d9e7103c5b6fff026
subhankhalid402 <subhan.khalid402@gmail.com>
2020-07-16 16:26:52 +0500

    Create 2020_07_01_122208_create_tasks_table.php

---
+++ tests/database/migrations/2020_07_01_122208_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('key');
+            $table->string('title');
+            $table->string('icon');
+
+            $table->timestamp('created_at', 0)->useCurrent();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
