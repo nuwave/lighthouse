9c05891cb16be50951f2d9b21339886114d1d2f1
Thiery Laverdure <thiery@spacestud.io>
2020-07-16 08:44:15 -0400

    Fix invalid type

--- src/Execution/DataLoader/RelationCountBatchLoader.php
+++ src/Execution/DataLoader/RelationCountBatchLoader.php
@@ -48,7 +48,7 @@ class RelationCountBatchLoader extends BatchLoader
      * Models are grouped by their fully qualified class name to prevent key
      * collisions between different types of models.
      *
-     * @return Collection<Model>
+     * @return Collection<\Illuminate\Database\Eloquent\Model>
      */
     protected function getParentModels(): Collection
     {
