75bc2beea199e402c4622fac7921b02c61aa59a7
Benedikt Franke <spawnia@users.noreply.github.com>
2020-08-05 18:19:54 +0000

    Apply fixes from StyleCI

--- src/Support/Pipeline.php
+++ src/Support/Pipeline.php
@@ -2,9 +2,7 @@
 
 namespace Nuwave\Lighthouse\Support;
 
-use Closure;
 use Illuminate\Pipeline\Pipeline as BasePipeline;
-use Illuminate\Support\Collection;
 
 class Pipeline extends BasePipeline
 {
