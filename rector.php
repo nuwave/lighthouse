47683b09b07689dcf92827c37b56199a746ffbce
Benedikt Franke <benedikt.franke@mll.com>
2020-08-17 21:41:05 +0200

    Apply style rules

--- rector.php
+++ rector.php
@@ -1,6 +1,7 @@
 <?php
 
 use Rector\Core\Configuration\Option;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedParameterRector;
 use Rector\Set\ValueObject\SetList;
 use Rector\SOLID\Rector\ClassMethod\UseInterfaceOverImplementationInConstructorRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -9,9 +10,15 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $services = $containerConfigurator->services();
 
-    $parameters->set(Option::SETS, [SetList::CODE_QUALITY]);
+    $parameters->set(Option::SETS, [
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+    ]);
 
     $parameters->set(Option::EXCLUDE_RECTORS, [
+        // Laravel classes are sometimes multi-purpose or magical
         UseInterfaceOverImplementationInConstructorRector::class,
+        // Having unused parameters can increase clarity, e.g. in event handlers
+        RemoveUnusedParameterRector::class,
     ]);
 };
