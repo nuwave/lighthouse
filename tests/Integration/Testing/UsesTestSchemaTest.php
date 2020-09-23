d7218e489bc303a5a078918521cfcfa35ea90722
Robson Tenório <rrtenorio@gmail.com>
2020-09-22 23:19:57 -0300

    Failing test case for trait  UsesTestSchema

---
+++ tests/Integration/Testing/UsesTestSchemaTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Integration\Schema\Directives;
+
+use Tests\DBTestCase;
+use Tests\Utils\Models\Category;
+use Tests\Utils\Models\Color;
+
+class UsesTestSchemaTest extends DBTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testCanSwitchSchemaOnSucessiveRequests(): void
+    {
+        factory(Color::class, 3)->create();
+        factory(Category::class, 4)->create();
+
+        $this->schema = '          
+            type Color {                
+                id: ID!
+                name: String!
+            }
+
+            type Query {
+                colors: [Color] @all
+            }
+            ';
+
+        $this->graphQL('
+            {
+                colors{
+                    id
+                    name
+                }
+            }
+            ')
+            ->assertJsonStructure([
+                'data' => ['colors']
+            ])
+            ->assertJsonCount(3, 'data.colors');
+
+
+        $this->schema = '          
+            type Color {                
+                id: ID!
+                name: String!
+            }
+
+            type Category {                
+                id: ID!
+                name: String!
+            }
+
+            type Query {
+                colors: [Color] @all
+                categories: [Category] @all
+            }
+            ';
+
+        $this->graphQL('
+            {
+                categories{
+                    id
+                    name
+                }
+            }
+            ')
+            ->assertJsonStructure([
+                'data' => ['categories']
+            ])
+            ->assertJsonCount(4, 'data.categories');
+    }
+}
