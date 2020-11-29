326b6abc16d851c9b5cbd3d943ee0d2bccfd7b94
Dylan Seidt <dylan.seidt@gmail.com>
2020-11-29 13:53:49 -0600

    Add optimized selects

---
+++ src/Select/SelectDirective.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Nuwave\Lighthouse\Select;
+
+use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
+
+class SelectDirective extends BaseDirective
+{
+    public static function definition(): string
+    {
+        return /** @lang GraphQL */ <<<'GRAPHQL'
+"""
+Specify the SQL column dependencies of this field.
+"""
+directive @select(
+  """
+  SQL columns names to pass to the Eloquent query builder
+  """
+  columns: [String!]
+) on FIELD_DEFINITION
+GRAPHQL;
+    }
+}
