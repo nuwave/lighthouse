c46a32d47b7b070ad6da2c7864a49eb02a3821b4
Alejandro Seisdedos <aseisdedos-externo@cnmc.es>
2020-12-02 11:28:00 +0100

    Add new test

--- tests/Unit/Support/AuthenticationTest.php
+++ tests/Unit/Support/AuthenticationTest.php
@@ -4,9 +4,10 @@ namespace Tests\Unit\Support;
 
 use InvalidArgumentException;
 use Nuwave\Lighthouse\Support\Authentication;
-use Tests\TestCase;
+use Tests\DBTestCase;
+use Tests\Utils\Models\User;
 
-class AuthenticationTest extends TestCase
+class AuthenticationTest extends DBTestCase
 {
     public function testGetDefaultConfigGuard(): void
     {
@@ -20,6 +21,18 @@ class AuthenticationTest extends TestCase
         $this->assertEquals(config('lighthouse.guard'), Authentication::getGuard());
     }
 
+    public function testGetCustomConfigGuard(): void
+    {
+        $customGuard = 'web';
+
+        config()->set('lighthouse.custom_guards', [$customGuard]);
+
+        $user = factory(User::class)->make();
+        $this->be($user);
+
+        $this->assertEquals($customGuard, Authentication::getGuard());
+    }
+
     public function testErrorIfNotExistsCustomGuard(): void
     {
         $customGuard = 'some_guard';
@@ -27,7 +40,7 @@ class AuthenticationTest extends TestCase
         config()->set('lighthouse.custom_guards', [$customGuard]);
 
         $this->expectException(InvalidArgumentException::class);
-        $expectedMessage = 'InvalidArgumentException: Auth guard ['.$customGuard.'] is not defined.';
+        $expectedMessage = 'InvalidArgumentException: Auth guard [' . $customGuard . '] is not defined.';
         $this->assertEquals($expectedMessage, Authentication::getGuard());
     }
 }
