b00a3b56295e0420f52d74e446e659a67b587435
Oliver Nybroe <oliver@worksome.com>
2020-12-11 08:53:14 +0100

    Add field builder directive

---
+++ tests/Integration/Execution/FieldBuilderDirectiveTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\Integration\Execution;
+
+use Tests\DBTestCase;
+use Tests\Utils\Models\Post;
+use Tests\Utils\Models\User;
+
+class FieldBuilderDirectiveTest extends DBTestCase
+{
+    protected $schema = /** @lang GraphQL */ '
+    type Post {
+        id: ID!
+        title: String
+    }
+    ';
+
+    public function testCanLimitPostByAuthenticatedUser(): void
+    {
+        $this->schema .= /** @lang GraphQL */ '
+        type Query {
+            posts: [Post!]! @all @whereAuth(relation: "user")
+        }
+        ';
+        $user = factory(User::class)->create();
+        $ownedPosts = factory(Post::class, 3)->create([
+            'user_id' => $user->getKey(),
+        ]);
+        $nonOwnedPosts = factory(Post::class, 3)->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->graphQL(/** @lang GraphQL */ '
+            query {
+                posts {
+                    id
+                }
+            }
+            ');
+
+        $this->assertCount(
+            0,
+            $ownedPosts
+                ->map(function (Post $post) {
+                    return $post->getKey();
+                })
+                ->diff($response->json('data.posts.*.id'))
+        );
+    }
+
+    public function testCanChangeGuard(): void
+    {
+        $this->schema .= /** @lang GraphQL */ '
+        type Query {
+            posts: [Post!]! @all @whereAuth(
+                relation: "user"
+                guard: "api"
+            )
+        }
+        ';
+        $user = factory(User::class)->create();
+        $ownedPosts = factory(Post::class, 3)->create([
+            'user_id' => $user->getKey(),
+        ]);
+        $nonOwnedPosts = factory(Post::class, 3)->create();
+
+        $this->app['auth']->guard('api')->setUser($user);
+
+        $response = $this
+            ->graphQL(/** @lang GraphQL */ '
+            query {
+                posts {
+                    id
+                }
+            }
+            ');
+
+        $this->assertCount(
+            0,
+            $ownedPosts
+                ->map(function (Post $post) {
+                    return $post->getKey();
+                })
+                ->diff($response->json('data.posts.*.id'))
+        );
+    }
+}
