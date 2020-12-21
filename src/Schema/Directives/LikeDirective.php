3487e30b6842f7cd63d470e45109f4ad7cd4ca30
Bruno <brunobg@gmail.com>
2020-12-21 11:09:11 -0300

    added @like directive

---
+++ src/Schema/Directives/LikeDirective.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Nuwave\Lighthouse\Schema\Directives;
+
+use Nuwave\Lighthouse\Support\Contracts\ArgBuilderDirective;
+
+class LikeDirective extends BaseDirective implements ArgBuilderDirective
+{
+    public static function definition(): string
+    {
+        return /** @lang GraphQL */ <<<'GRAPHQL'
+enum LikePercentageLocation {
+    START
+    END
+    BOTH
+}
+
+"""
+Uses a LIKE query.
+"""
+directive @like(
+  """
+  Specify the database column to compare.
+  Only required if database column has a different name than the attribute in your schema.
+  """
+  key: String
+
+  """
+  Specify the positions of the % in the LIKE comparison. The default is BOTH.
+  """
+  percentage: LikePercentageLocation
+) repeatable on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION
+GRAPHQL;
+    }
+
+    protected function escapePercentage(string $value, string $char = '\\'): string
+    {
+        return str_replace(
+            [$char, '%', '_'],
+            [$char . $char, $char . '%', $char . '_'],
+            $value
+        );
+    }
+
+    /**
+     * Apply a "WHERE LIKE $value" clause.
+     *
+     * @param  \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder  $builder
+     * @return \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder
+     */
+    public function handleBuilder($builder, $value): object
+    {
+        $valueEscaped = $this->escapePercentage($value);
+
+        $percentage = $this->directiveArgValue('key', $this->nodeName(), 'BOTH');
+        switch ($percentage) {
+            case 'START':
+                $valueEscaped = '%' . $valueEscaped;
+                break;
+            case 'END':
+                $valueEscaped = $valueEscaped . '%';
+                break;
+            case 'BOTH';
+                $valueEscaped = '%' . $valueEscaped . '%';
+                break;
+        }
+
+        return $builder->where(
+            $this->directiveArgValue('key', $this->nodeName()),
+            'LIKE',
+            $valueEscaped
+        );
+    }
+}
---
+++ tests/Integration/Schema/Directives/LikeDirectiveTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Tests\Integration\Schema\Directives;
+
+use Tests\DBTestCase;
+use Tests\Utils\Models\Company;
+use Tests\Utils\Models\User;
+
+class LikeDirectiveTest extends DBTestCase
+{
+    public function testLikeEnd(): void
+    {
+        $userA = factory(User::class)->create(['name' => 'Alan']);
+        $userB = factory(User::class)->create(['name' => 'Alex']);
+        $userC = factory(User::class)->create(['name' => 'Aaron']);
+
+        $this->schema = /** @lang GraphQL */ '
+        type User {
+            id: ID!
+            name: String!
+        }
+
+        type Query {
+            users(name: String! @like(percentage: END): [User!]
+        }
+        ';
+
+        $this->graphQL(/** @lang GraphQL */ "
+        {
+            user(name: \"Al\") {
+                name
+            }
+        }
+        ")->assertJsonFragment([
+            'users' => [
+                [
+                    'name' => 'Alan',
+                ],
+                [
+                    'name' => 'Alex',
+                ]
+            ],
+        ]);
+    }
+
+    public function testLikeBoth(): void
+    {
+        $userA = factory(User::class)->create(['name' => 'Alan']);
+        $userB = factory(User::class)->create(['name' => 'Alex']);
+        $userC = factory(User::class)->create(['name' => 'Aaron']);
+
+        $this->schema = /** @lang GraphQL */ '
+        type User {
+            id: ID!
+            name: String!
+        }
+
+        type Query {
+            users(name: String! @like(percentage: BOTH): [User!]
+        }
+        ';
+
+        $this->graphQL(/** @lang GraphQL */ "
+        {
+            user(name: \"l\") {
+                name
+            }
+        }
+        ")->assertJsonFragment([
+            'users' => [
+                [
+                    'name' => 'Alan',
+                ],
+                [
+                    'name' => 'Alex',
+                ]
+            ],
+        ]);
+    }
+
+    public function testLikeEscape(): void
+    {
+        $userA = factory(User::class)->create(['name' => 'Alan']);
+        $userB = factory(User::class)->create(['name' => 'Alex']);
+        $userC = factory(User::class)->create(['name' => 'Aar%on']);
+
+        $this->schema = /** @lang GraphQL */ '
+        type User {
+            id: ID!
+            name: String!
+        }
+
+        type Query {
+            users(name: String! @like(percentage: BOTH): [User!]
+        }
+        ';
+
+        $this->graphQL(/** @lang GraphQL */ "
+        {
+            user(name: \"ar%\") {
+                name
+            }
+        }
+        ")->assertJsonFragment([
+            'users' => [
+                [
+                    'name' => 'Aar%on',
+                ]
+            ],
+        ]);
+    }
+
+}
