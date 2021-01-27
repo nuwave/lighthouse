d8e88f8b669aa2bf8ab1237e2e7e436dad339834
Benedikt Franke <benedikt@franke.tech>
2021-01-27 22:25:13 +0100

    Add test

---
+++ tests/Integration/Schema/Directives/LazyLoadDirectiveTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Integration\Schema\Directives;
+
+use Nuwave\Lighthouse\Exceptions\DefinitionException;
+use Tests\DBTestCase;
+use Tests\Utils\Models\User;
+
+class LazyLoadDirectiveTest extends DBTestCase
+{
+    public function testLazyLoadRequiresRelationArgument(): void
+    {
+        $this->expectException(DefinitionException::class);
+
+        $this->buildSchema(/** @lang GraphQL */ '
+        type Query {
+            foo: ID @lazyLoad
+        }
+        ');
+    }
+
+    public function testLazyLoadRelationArgumentMustNotBeEmptyList(): void
+    {
+        $this->expectException(DefinitionException::class);
+
+        $this->buildSchema(/** @lang GraphQL */ '
+        type Query {
+            foo: ID @lazyLoad(relations: [])
+        }
+        ');
+    }
+}
