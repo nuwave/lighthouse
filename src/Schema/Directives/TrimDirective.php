2a032e9b793a59abe4508910822dd38e08809b34
spawnia <benedikt@franke.tech>
2021-02-07 19:27:33 +0100

    Revert "wip"
    This reverts commit e6df78cc

--- src/Schema/Directives/TrimDirective.php
+++ src/Schema/Directives/TrimDirective.php
@@ -51,7 +51,6 @@ GRAPHQL;
             $fieldValue->setResolver(
                 function ($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) use ($resolver) {
                     dump($resolveInfo->path);
-                    // TODO when we are in a nested field that is inside a list, this is run multiple times
                     $resolveInfo->argumentSet = $this->transformArgumentSet($resolveInfo->argumentSet);
 
                     return $resolver(
