81e121c80ea77c088a2baeef7844cf0708ec5c75
spawnia <benedikt@franke.tech>
2021-02-07 19:48:35 +0100

    Test proper events are fired

--- tests/Integration/LifecycleEventsTest.php
+++ tests/Integration/LifecycleEventsTest.php
@@ -3,40 +3,48 @@
 namespace Tests\Integration;
 
 use Illuminate\Contracts\Events\Dispatcher as EventsDispatcher;
+use Illuminate\Support\Str;
+use Nuwave\Lighthouse\Events\BuildExtensionsResponse;
+use Nuwave\Lighthouse\Events\BuildSchemaString;
+use Nuwave\Lighthouse\Events\EndExecution;
+use Nuwave\Lighthouse\Events\EndRequest;
+use Nuwave\Lighthouse\Events\ManipulateAST;
+use Nuwave\Lighthouse\Events\ManipulateResult;
+use Nuwave\Lighthouse\Events\StartExecution;
+use Nuwave\Lighthouse\Events\StartRequest;
 use Tests\TestCase;
-use Tests\Utils\Queries\Foo;
 
 class LifecycleEventsTest extends TestCase
 {
-    protected $schema = /** @lang GraphQL */ '
-    type Query {
-        foo: Int
-    }
-    ';
-
     public function testDispatchesProperLifecycleEvents(): void
     {
         /** @var \Illuminate\Contracts\Events\Dispatcher $eventsDispatcher */
         $eventsDispatcher = app(EventsDispatcher::class);
 
+        /** @var array<int, object> $events */
         $events = [];
 
-        $eventsDispatcher->listen('*', function ($event) use ($events) {
-            $events []= $event;
+        $eventsDispatcher->listen('*', function (string $name, array $payload) use (&$events) {
+            if (Str::startsWith($name, 'Nuwave\\Lighthouse')) {
+                // We only fire class-based events, so the payload
+                // always holds exactly a single class instance.
+                $events []= $payload[0];
+            }
         });
 
-        $this
-            ->graphQL(/** @lang GraphQL */ '
-            {
-                foo
-            }
-            ')
-            ->assertExactJson([
-                'data' => [
-                    'foo' => Foo::THE_ANSWER,
-                ],
-            ]);
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            foo
+        }
+        ');
 
-        dump($events);
+        $this->assertInstanceOf(StartRequest::class, $events[0]);
+        $this->assertInstanceOf(BuildSchemaString::class, $events[1]);
+        $this->assertInstanceOf(ManipulateAST::class, $events[2]);
+        $this->assertInstanceOf(StartExecution::class, $events[3]);
+        $this->assertInstanceOf(BuildExtensionsResponse::class, $events[4]);
+        $this->assertInstanceOf(ManipulateResult::class, $events[5]);
+        $this->assertInstanceOf(EndExecution::class, $events[6]);
+        $this->assertInstanceOf(EndRequest::class, $events[7]);
     }
 }
