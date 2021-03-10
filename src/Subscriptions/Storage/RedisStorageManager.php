ab80dd8f2b7697df1d85114670c7d23f04945a05
Konstantin Duczek <konz.duczek@gmail.com>
2021-03-10 12:47:59 +0100

    Fix mget call with empty subscribers (#1759)

--- src/Subscriptions/Storage/RedisStorageManager.php
+++ src/Subscriptions/Storage/RedisStorageManager.php
@@ -60,6 +60,9 @@ class RedisStorageManager implements StoresSubscriptions
         // As explained in storeSubscriber, we use redis sets to store the names of subscribers of a topic.
         // We can retrieve all members of a set using the command smembers.
         $subscriberIds = $this->connection->command('smembers', [$this->topicKey($topic)]);
+        if (count($subscriberIds) === 0) {
+            return new Collection();
+        }
 
         // Since we store the individual subscribers with a prefix,
         // but not in the set, we have to add the prefix here.
