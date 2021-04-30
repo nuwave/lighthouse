5f7ad68d9685ca637246894fdac5d53c3c6081e5
hasta <hastashakiba@gmail.com>
2021-04-30 11:46:39 +0430

    add withMin Directive

---
+++ src/Schema/Directives/WithMinDirective.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Nuwave\Lighthouse\Schema\Directives;
+
+use GraphQL\Language\AST\FieldDefinitionNode;
+use GraphQL\Language\AST\ObjectTypeDefinitionNode;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Exceptions\DefinitionException;
+use Nuwave\Lighthouse\Execution\DataLoader\RelationMinLoader;
+use Nuwave\Lighthouse\Schema\AST\DocumentAST;
+use Nuwave\Lighthouse\Schema\RootType;
+use Nuwave\Lighthouse\Support\Contracts\FieldManipulator;
+use Nuwave\Lighthouse\Support\Contracts\FieldMiddleware;
+use Nuwave\Lighthouse\Execution\DataLoader\RelationAggregateLoader;
+
+class WithMinDirective extends WithRelationAggregateDirective implements FieldMiddleware, FieldManipulator
+{
+    public static function definition(): string
+    {
+        return /** @lang GraphQL */ <<<'GRAPHQL'
+"""
+Eager-load the min of an Eloquent relation if the field is queried.
+
+Note that this does not return a value for the field, the count is simply
+prefetched, assuming it is used to compute the field value. Use `@count`
+if the field should simply return the relation count.
+"""
+directive @withMin(
+  """
+  Specify the relationship method name in the model class.
+  """
+  relation: String!
+
+  """
+  Specify the relationship column name in the model class.
+  """
+  column: String!
+
+  """
+  Apply scopes to the underlying query.
+  """
+  scopes: [String!]
+) repeatable on FIELD_DEFINITION
+GRAPHQL;
+    }
+
+    public function manipulateFieldDefinition(DocumentAST &$documentAST, FieldDefinitionNode &$fieldDefinition, ObjectTypeDefinitionNode &$parentType)
+    {
+        if (RootType::isRootType($parentType->name->value)) {
+            throw new DefinitionException("Can not use @{$this->name()} on fields of a root type.");
+        }
+
+        $relation = $this->directiveArgValue('relation');
+        if (! is_string($relation)) {
+            throw new DefinitionException("You must specify the argument relation in the {$this->name()} directive on {$this->definitionNode->name->value}.");
+        }
+    }
+
+    protected function relationName(): string
+    {
+        /**
+         * We validated the argument during schema manipulation.
+         *
+         * @var string $relation
+         */
+        $relation = $this->directiveArgValue('relation');
+
+        return $relation;
+    }
+
+    protected function relationColumn(): string
+    {
+        /**
+         * @var string $column
+         */
+        $column = $this->directiveArgValue('column');
+
+        return $column;
+    }
+
+    /**
+     * @return RelationMinLoader
+     */
+    protected function relationAggregateLoader(ResolveInfo $resolveInfo): RelationAggregateLoader
+    {
+        return new RelationMinLoader(
+            $this->decorateBuilder($resolveInfo)
+        );
+    }
+}
---
+++ tests/Integration/Schema/Directives/WithMinDirectiveTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tests\Integration\Schema\Directives;
+
+use Tests\Utils\Models\Book;
+use Illuminate\Support\Facades\DB;
+use Tests\DBTestCase;
+use Tests\Utils\Models\Author;
+use function factory;
+
+class WithMinDirectiveTest extends DBTestCase
+{
+    public function testEagerLoadsRelationMin(): void
+    {
+        $this->schema = /** @lang GraphQL */ '
+        type Query {
+            authors: [Author!] @all
+        }
+
+        type Book {
+            title: String!
+            price: Int!
+        }
+
+        type Author {
+            name: String!
+            books:[Book!]! @belongsToMany
+            books_min_price: Int!
+                @withMin(relation: "books", column: "price")
+        }
+        ';
+
+        [$author1,$author2,$author3]=factory(Author::class, 3)->create();
+        $book1= factory(Book::class)->create(["price"=>10]);
+        $book2= factory(Book::class)->create(["price"=>20]);
+        $book3= factory(Book::class)->create(["price"=>30]);
+        $author1->books()->attach([
+            $book1->id, $book2->id,
+        ]);
+        $author2->books()->attach([
+            $book2->id, $book3->id,
+        ]);
+        $author3->books()->attach([
+            $book1->id, $book2->id, $book3->id,
+        ]);
+        $queries = 0;
+        DB::listen(function ($q) use (&$queries): void {
+            $queries++;
+        });
+
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            authors {
+                books_min_price
+            }
+        }
+        ')->assertExactJson([
+            'data' => [
+                'authors' => [
+                    [
+                        'books_min_price' => 10,
+                    ],
+                    [
+                        'books_min_price' => 20,
+                    ],
+                    [
+                        'books_min_price' => 10,
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->assertSame(2, $queries);
+    }
+}
