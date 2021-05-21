ef2ecf3d13720a0f8a0de8d0b98ef97a507ac7b1
Benedikt Franke <spawnia@users.noreply.github.com>
2021-05-21 11:53:58 +0000

    Apply fixes from StyleCI

--- src/Events/StartOperationOrOperations.php
+++ src/Events/StartOperationOrOperations.php
@@ -10,7 +10,7 @@ class StartOperationOrOperations
     /**
      * One or multiple parsed GraphQL operations.
      *
-     * @var \GraphQL\Server\OperationParams|array<int, \GraphQL\Server\OperationParams> $operationOrOperations
+     * @var \GraphQL\Server\OperationParams|array<int, \GraphQL\Server\OperationParams>
      */
     public $operationOrOperations;
 
