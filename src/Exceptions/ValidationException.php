7fb9d483e665308c0f50f8f4a523f9f8a2ed7d03
Rahul Dey <rahul900day@gmail.com>
2021-05-25 01:26:21 +0530

    Update ValidationException.php

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -2,8 +2,10 @@
 
 namespace Nuwave\Lighthouse\Exceptions;
 
+use Illuminate\Support\Arr;
 use Exception;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Support\Facades\Validator as ValidatorFacade;
 
 class ValidationException extends Exception implements RendersErrorsExtensions
 {
@@ -29,6 +31,19 @@ class ValidationException extends Exception implements RendersErrorsExtensions
     public function getCategory(): string
     {
         return self::CATEGORY;
+    }
+    
+    public static function withMessage(array $messages)
+    {
+        $validator = tap(ValidatorFacade::make([], []), function($validator) use ($messages) {
+            foreach ($messages as $key => $value) {
+                foreach (Arr::wrap($value) as $message) {
+                    $validator->errors()->add($key, $message);
+                }
+            }
+        });
+
+        return new static('Validation Failed', $validator);
     }
 
     public function extensionsContent(): array
