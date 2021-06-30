e6b52a9fbfe4005f2d8b64858680251c4250dac7
Benedikt Franke <spawnia@users.noreply.github.com>
2021-06-30 10:58:41 +0000

    Apply fixes from StyleCI

--- tests/Console/CacheCommandTest.php
+++ tests/Console/CacheCommandTest.php
@@ -8,9 +8,9 @@ use Illuminate\Filesystem\Filesystem;
 use Nuwave\Lighthouse\Console\CacheCommand;
 use Nuwave\Lighthouse\Exceptions\UnknownCacheVersionException;
 use Nuwave\Lighthouse\Schema\AST\DocumentAST;
+use Tests\TestCase;
 use Tests\TestsSchemaCache;
 use Tests\TestsSerialization;
-use Tests\TestCase;
 
 class CacheCommandTest extends TestCase
 {
