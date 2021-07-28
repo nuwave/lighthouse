5da4001c5fd5b09728deec477ea6c912bcc73e32
Malico <hi@malico.me>
2021-07-28 19:56:36 +0100

    Add tests for multi Inheritance

--- tests/Integration/Schema/Directives/InheritsDirectiveTest.php
+++ tests/Integration/Schema/Directives/InheritsDirectiveTest.php
@@ -25,6 +25,31 @@ class InheritsDirectiveTest extends TestCase
         $this->assertNotNull($childType->getField('attribute_1'));
     }
 
+    public function testIfItcanInheritFromThatInherited()
+    {
+        $schema = $this->buildSchema(/**@lang GraphQL */ '
+            type GrandParentType {
+                attribute_1: String
+            }
+            type ParentType @inherits(from: GrandParentType) {
+                attribute_2: String
+            }
+
+            type ChildType @inherits(from: ParentType){
+                attribute_3: String
+            }
+
+            type Query {
+                childtypeQuery: ChildType
+            }');
+
+        $childType = $schema->getType('ChildType');
+
+        $this->assertNotNull($childType->getField('attribute_1'));
+        $this->assertNotNull($childType->getField('attribute_2'));
+        $this->assertNotNull($childType->getField('attribute_3'));
+    }
+
     public function testChildOverridesFields()
     {
         $schema = $this->buildSchema(/* @lang GraphQL */
