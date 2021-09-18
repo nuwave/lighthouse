da3403bc6f25cc3969a694496ad2401dab637f80
Aleksei Lebedev <1329824+LastDragon-ru@users.noreply.github.com>
2021-09-18 22:12:11 +0400

    Both tests for `SpreadDirective` combined into one.

--- tests/Unit/Schema/Directives/SpreadDirectiveTest.php
+++ tests/Unit/Schema/Directives/SpreadDirectiveTest.php
@@ -12,41 +12,7 @@ class SpreadDirectiveTest extends TestCase
             ->with(null, [
                 'foo' => 1,
                 'baz' => 2,
-            ]);
-
-        $this->schema = /** @lang GraphQL */ '
-        type Query {
-            foo(input: Foo @spread): Int @mock
-        }
-
-        input Foo {
-            foo: Int
-            bar: Bar @spread
-        }
-
-        input Bar {
-            baz: Int
-        }
-        ';
-
-        $this->graphQL(/** @lang GraphQL */ '
-        {
-            foo(input: {
-                foo: 1
-                bar: {
-                    baz: 2
-                }
-            })
-        }
-        ');
-    }
-
-    public function testNestedSpreadWithResolver(): void
-    {
-        $this->mockResolver()
-            ->with(null, [
-                'foo' => 1,
-                'bar__baz' => 2,
+                'qux__baz' => 3,
             ]);
 
         $resolver = $this->qualifyTestResolver('spread');
@@ -57,7 +23,8 @@ class SpreadDirectiveTest extends TestCase
 
         input Foo {
             foo: Int
-            bar: Bar @spread(resolver: \"{$resolver}\")
+            bar: Bar @spread
+            qux: Bar @spread(resolver: \"{$resolver}\")
         }
 
         input Bar {
@@ -71,6 +38,9 @@ class SpreadDirectiveTest extends TestCase
                 foo: 1
                 bar: {
                     baz: 2
+                }
+                qux: {
+                    baz: 3
                 }
             })
         }
