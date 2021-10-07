048cc349b128265f3c8fc63b8a68812ab55ab96e
Benedikt Franke <benedikt.franke@mll.com>
2021-10-07 15:48:56 +0200

    todo

--- src/Support/Contracts/ComplexityResolverDirective.php
+++ src/Support/Contracts/ComplexityResolverDirective.php
@@ -4,6 +4,10 @@ namespace Nuwave\Lighthouse\Support\Contracts;
 
 use Nuwave\Lighthouse\Schema\Values\FieldValue;
 
+/**
+ * TODO implement this interface in resolver directives where it makes sense, e.g. @paginate.
+ * This can only be done in v6, as this can be a breaking change.
+ */
 interface ComplexityResolverDirective extends Directive
 {
     /**
