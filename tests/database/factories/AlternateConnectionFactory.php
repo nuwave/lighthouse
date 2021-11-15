4265da1b6e7736b5853d12ecdd1406b17ec75f06
Benedikt Franke <benedikt@franke.tech>
2021-11-15 22:36:40 +0100

    Dont batchload relations with different database connections (#1979)

---
+++ tests/database/factories/AlternateConnectionFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+use Tests\Utils\Models\AlternateConnection;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(AlternateConnection::class, function (Faker $faker): array {
+    return [];
+});
