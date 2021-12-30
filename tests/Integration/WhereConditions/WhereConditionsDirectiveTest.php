f1f9518d7d820ddfaa6dc0cc61eb52896c62be4e
spawnia <benedikt@franke.tech>
2021-12-30 13:16:37 +0100

    clean up tests

--- tests/Integration/WhereConditions/WhereConditionsDirectiveTest.php
+++ tests/Integration/WhereConditions/WhereConditionsDirectiveTest.php
@@ -15,37 +15,6 @@ use Tests\Utils\Models\User;
 
 class WhereConditionsDirectiveTest extends DBTestCase
 {
-    protected $schema = /** @lang GraphQL */ '
-    type User {
-        id: ID!
-        name: String
-        email: String
-    }
-
-    type Post {
-        id: ID!
-        title: String
-        body: String
-        parent: Post @belongsTo
-    }
-
-    type Query {
-        posts(where: _ @whereConditions): [Post!]! @all
-        users(where: _ @whereConditions): [User!]! @all
-        whitelistedColumns(
-            where: _ @whereConditions(columns: ["id", "camelCase"])
-        ): [User!]! @all
-        enumColumns(
-            where: _ @whereConditions(columnsEnum: "UserColumn")
-        ): [User!]! @all
-    }
-
-    enum UserColumn {
-        ID @enum(value: "id")
-        NAME @enum(value: "name")
-    }
-    ';
-
     protected function getPackageProviders($app): array
     {
         return array_merge(
@@ -56,6 +25,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testDefaultsToWhereEqual(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 2)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
@@ -74,6 +53,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testOverwritesTheOperator(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 3)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
@@ -93,6 +82,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testOperatorIn(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 5)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
@@ -123,6 +122,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testOperatorIsNull(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type Post {
+            id: ID!
+        }
+
+        type Query {
+            posts(where: _ @whereConditions): [Post!]! @all
+        }
+        ';
+
         factory(Post::class)->create([
             'body' => null,
         ]);
@@ -154,6 +163,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testOperatorNotNull(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type Post {
+            id: ID!
+        }
+
+        type Query {
+            posts(where: _ @whereConditions): [Post!]! @all
+        }
+        ';
+
         factory(Post::class)->create([
             'body' => null,
         ]);
@@ -185,6 +204,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testOperatorNotBetween(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 5)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
@@ -215,6 +244,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testAddsNestedAnd(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 3)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
@@ -243,6 +282,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testAddsNestedOr(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 5)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
@@ -292,6 +341,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testAddsNestedAndOr(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 5)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
@@ -339,6 +398,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testHasMixed(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 9)->create()->each(function ($user) {
             $user->posts()->saveMany(factory(Post::class, 2)->create());
         });
@@ -423,6 +492,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testHasRelation(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 5)->create()->each(function ($user) {
             $user->posts()->saveMany(factory(Post::class, 2)->create());
         });
@@ -467,6 +546,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testHasAmount(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 5)->create()->each(function ($user) {
             $user->posts()->saveMany(factory(Post::class, 2)->create());
         });
@@ -513,6 +602,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testHasOperator(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 5)->create()->each(function ($user) {
             $user->posts()->saveMany(factory(Post::class, 2)->create());
         });
@@ -560,6 +659,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testHasCondition(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 5)->create()->each(function ($user) {
             $user->posts()->saveMany(factory(Post::class, 2)->create());
         });
@@ -605,6 +714,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testHasRecursive(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 7)->create()->each(function ($user) {
             $user->posts()->saveMany(factory(Post::class, 2)->create());
         });
@@ -651,6 +770,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testHasNested(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 5)->create()->each(function ($user) {
             $user->posts()->saveMany(factory(Post::class, 2)->create());
         });
@@ -707,6 +836,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testRejectsInvalidColumnName(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         $this->graphQL(/** @lang GraphQL */ '
         {
             users(
@@ -727,11 +866,27 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testQueriesEmptyStrings(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 3)->create();
 
-        $userNamedEmptyString = factory(User::class)->create([
-            'name' => '',
-        ]);
+        /** @var \Tests\Utils\Models\User $userNamedEmptyString */
+        $userNamedEmptyString = factory(User::class)->make();
+        $userNamedEmptyString->name = '';
+        $userNamedEmptyString->save();
+
+        /** @var \Tests\Utils\Models\User $userNamedNull */
+        $userNamedNull = factory(User::class)->make();
+        $userNamedNull->name = null;
+        $userNamedNull->save();
 
         $this->graphQL(/** @lang GraphQL */ '
         {
@@ -742,7 +897,6 @@ class WhereConditionsDirectiveTest extends DBTestCase
                 }
             ) {
                 id
-                name
             }
         }
         ')->assertJson([
@@ -750,7 +904,6 @@ class WhereConditionsDirectiveTest extends DBTestCase
                 'users' => [
                     [
                         'id' => $userNamedEmptyString->id,
-                        'name' => $userNamedEmptyString->name,
                     ],
                 ],
             ],
@@ -759,11 +912,27 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testQueryForNull(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         factory(User::class, 3)->create();
 
-        $userNamedNull = factory(User::class)->create([
-            'name' => null,
-        ]);
+        /** @var \Tests\Utils\Models\User $userNamedEmptyString */
+        $userNamedEmptyString = factory(User::class)->make();
+        $userNamedEmptyString->name = '';
+        $userNamedEmptyString->save();
+
+        /** @var \Tests\Utils\Models\User $userNamedNull */
+        $userNamedNull = factory(User::class)->make();
+        $userNamedNull->name = null;
+        $userNamedNull->save();
 
         $this->graphQL(/** @lang GraphQL */ '
         {
@@ -774,7 +943,6 @@ class WhereConditionsDirectiveTest extends DBTestCase
                 }
             ) {
                 id
-                name
             }
         }
         ')->assertJson([
@@ -782,7 +950,6 @@ class WhereConditionsDirectiveTest extends DBTestCase
                 'users' => [
                     [
                         'id' => $userNamedNull->id,
-                        'name' => $userNamedNull->name,
                     ],
                 ],
             ],
@@ -791,6 +958,16 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testRequiresAValueForAColumn(): void
     {
+        $this->schema = /** @lang GraphQL */'
+        type User {
+            id: ID!
+        }
+
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
         $this->graphQL(/** @lang GraphQL */ '
         {
             users(
@@ -806,6 +983,18 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testOnlyAllowsWhitelistedColumns(): void
     {
+        $this->schema = /** @lang GraphQL */ '
+        type User {
+            id: ID!
+        }
+        
+        type Query {
+            whitelistedColumns(
+                where: _ @whereConditions(columns: ["id", "camelCase"])
+            ): [User!]! @all
+        }
+        ';
+
         factory(User::class)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
@@ -866,6 +1055,23 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testUseColumnEnumsArg(): void
     {
+        $this->schema = /** @lang GraphQL */ '
+        type User {
+            id: ID!
+        }
+        
+        type Query {
+            enumColumns(
+                where: _ @whereConditions(columnsEnum: "UserColumn")
+            ): [User!]! @all
+        }
+        
+        enum UserColumn {
+            ID @enum(value: "id")
+            NAME @enum(value: "name")
+        }
+        ';
+
         factory(User::class)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
@@ -892,7 +1098,17 @@ class WhereConditionsDirectiveTest extends DBTestCase
 
     public function testIgnoreNullCondition(): void
     {
-        factory(User::class)->create();
+        $this->schema = /** @lang GraphQL */ '
+        type User {
+            id: ID!
+        }
+        
+        type Query {
+            users(where: _ @whereConditions): [User!]! @all
+        }
+        ';
+
+        $user = factory(User::class)->create();
 
         $this->graphQL(/** @lang GraphQL */ '
         {
@@ -906,7 +1122,7 @@ class WhereConditionsDirectiveTest extends DBTestCase
             'data' => [
                 'users' => [
                     [
-                        'id' => '1',
+                        'id' => "{$user->id}",
                     ],
                 ],
             ],
@@ -962,7 +1178,7 @@ class WhereConditionsDirectiveTest extends DBTestCase
         type User {
             id: ID!
         }
-    
+
         type Query {
             users(where: _ @whereConditions(
                 columns: ["name"],
