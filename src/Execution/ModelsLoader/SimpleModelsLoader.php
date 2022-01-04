7af3e93eb4553b7dcb89dccb55c1526a639098b2
Benedikt Franke <benedikt.franke@mll.com>
2022-01-04 14:16:47 +0100

    Store under unique key

--- src/Execution/ModelsLoader/SimpleModelsLoader.php
+++ src/Execution/ModelsLoader/SimpleModelsLoader.php
@@ -5,6 +5,7 @@ namespace Nuwave\Lighthouse\Execution\ModelsLoader;
 use Closure;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class SimpleModelsLoader implements ModelsLoader
 {
@@ -18,15 +19,35 @@ class SimpleModelsLoader implements ModelsLoader
      */
     protected $decorateBuilder;
 
+    /**
+     * Unique key under which to store the relation to eliminate duplicates.
+     *
+     * @var string
+     */
+    protected $key;
+
     public function __construct(string $relation, Closure $decorateBuilder)
     {
         $this->relation = $relation;
         $this->decorateBuilder = $decorateBuilder;
+        $this->key = Str::uuid()->toString();
     }
 
     public function load(EloquentCollection $parents): void
     {
+        // Dot notation may be used to eager load nested relations
+        $parts = explode('.', $this->relation);
+
+        // We just return the first level of relations for now.
+        // They hold the nested relations in case they are needed.
+        $firstRelation = $parts[0];
+
         $parents->load([$this->relation => $this->decorateBuilder]);
+
+        foreach ($parents as $model) {
+            $relation = $model->getRelation($firstRelation);
+            $model->setRelation($this->key, $relation);
+        }
     }
 
     /**
@@ -36,13 +57,6 @@ class SimpleModelsLoader implements ModelsLoader
      */
     public function extract(Model $model)
     {
-        // Dot notation may be used to eager load nested relations
-        $parts = explode('.', $this->relation);
-
-        // We just return the first level of relations for now.
-        // They hold the nested relations in case they are needed.
-        $firstRelation = $parts[0];
-
-        return $model->getRelation($firstRelation);
+        return $model->getRelation($this->key);
     }
 }
