4d86dfe75132a187f326159e9874b526470d23ca
Benedikt Franke <benedikt.franke@mll.com>
2022-01-11 16:55:27 +0100

    doc

--- src/Support/Contracts/ProvidesValidationRules.php
+++ src/Support/Contracts/ProvidesValidationRules.php
@@ -8,9 +8,9 @@ interface ProvidesValidationRules
      * A set of rules for query validation step.
      *
      * Returning `null` enables all available rules.
-     * Empty array would allow to skip query validation.
+     * Empty array skips query validation entirely.
      *
-     * @return array<class-string<\GraphQL\Validator\Rules\ValidationRule>, \GraphQL\Validator\Rules\ValidationRule>|null
+     * @return array<string, \GraphQL\Validator\Rules\ValidationRule>|null
      */
     public function validationRules(): ?array;
 }
