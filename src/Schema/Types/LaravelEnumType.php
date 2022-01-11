3a1286fa134c796bc02ccde7f4287bbc19ec76b1
Benedikt Franke <benedikt@franke.tech>
2022-01-11 15:04:30 +0100

    Allow marking `enum` values derived from `bensampo/laravel-enum` as `@deprecated` through PHPDoc (#2025)

--- src/Schema/Types/LaravelEnumType.php
+++ src/Schema/Types/LaravelEnumType.php
@@ -3,18 +3,27 @@
 namespace Nuwave\Lighthouse\Schema\Types;
 
 use BenSampo\Enum\Enum;
+use GraphQL\Type\Definition\Directive;
 use GraphQL\Type\Definition\EnumType;
 use InvalidArgumentException;
+use function Safe\substr;
 
 /**
  * A convenience wrapper for registering enums programmatically.
  */
 class LaravelEnumType extends EnumType
 {
+    public const DEPRECATED_PHPDOC_TAG = '@deprecated';
+
     /**
      * @var class-string<\BenSampo\Enum\Enum>
      */
     protected $enumClass;
+
+    /**
+     * @var \ReflectionClass<\BenSampo\Enum\Enum>
+     */
+    protected $reflection;
 
     /**
      * Create a GraphQL enum from a Laravel enum type.
@@ -31,6 +40,7 @@ class LaravelEnumType extends EnumType
         }
 
         $this->enumClass = $enumClass;
+        $this->reflection = new \ReflectionClass($enumClass);
 
         parent::__construct([
             'name' => $name ?? class_basename($enumClass),
@@ -43,11 +53,43 @@ class LaravelEnumType extends EnumType
                         'name' => $enum->key,
                         'value' => $enum,
                         'description' => $enum->description,
+                        'deprecationReason' => $this->deprecationReason($enum),
                     ];
                 },
                 $enumClass::getInstances()
             ),
         ]);
+    }
+
+    protected function deprecationReason(Enum $enum): ?string
+    {
+        $constant = $this->reflection->getReflectionConstant($enum->key);
+        assert($constant instanceof \ReflectionClassConstant, 'Enum keys are derived from the constant names');
+
+        $docComment = $constant->getDocComment();
+        if (false === $docComment) {
+            return null;
+        }
+
+        $docComment = substr($docComment, 3); // strip leading /**
+        $docComment = substr($docComment, 0, -2); // strip trailing */
+
+        $lines = explode("\n", $docComment);
+        foreach ($lines as $line) {
+            $parts = explode(self::DEPRECATED_PHPDOC_TAG, $line);
+
+            if (1 === count($parts)) {
+                continue;
+            }
+
+            $reason = trim($parts[1]);
+
+            return '' === $reason
+                ? Directive::DEFAULT_DEPRECATION_REASON
+                : $reason;
+        }
+
+        return null;
     }
 
     /**
