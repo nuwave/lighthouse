a47a3c28791016598eeb1b34d3ba0145a7901259
Benedikt Franke <benedikt.franke@mll.com>
2022-01-19 10:58:06 +0100

    Test and improve default schema

---
+++ tests/Integration/DefaultSchemaTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Integration;
+
+use Tests\DBTestCase;
+
+class DefaultSchemaTest extends DBTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->schema = file_get_contents(__DIR__ . '/../../src/default-schema.graphql');
+    }
+
+    public function testEmptyUsers(): void
+    {
+        $this
+            ->graphQL(/** @lang GraphQL */ '
+            {
+                users {
+                    id
+                }
+            }
+            ')
+            ->assertExactJson([
+                'data' => [
+                    'users' => [],
+                ],
+            ]);
+    }
+}
