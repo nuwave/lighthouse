831ff950c56dfa67b5133db9490f2c98f2587562
Benedikt Franke <benedikt.franke@mll.com>
2022-01-25 16:29:07 +0100

    Combine relation loading in nested toMany relations

--- src/Execution/Arguments/UpdateModel.php
+++ src/Execution/Arguments/UpdateModel.php
@@ -3,6 +3,7 @@
 namespace Nuwave\Lighthouse\Execution\Arguments;
 
 use GraphQL\Error\Error;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Nuwave\Lighthouse\Support\Contracts\ArgResolver;
 
@@ -24,10 +25,23 @@ class UpdateModel implements ArgResolver
     }
 
     /**
-     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  Model  $model
      * @param  \Nuwave\Lighthouse\Execution\Arguments\ArgumentSet  $args
      */
     public function __invoke($model, $args)
+    {
+        $id = self::pullId($args, $model);
+        $instance = $model->newQuery()->findOrFail($id);
+
+        return ($this->previous)($instance, $args);
+    }
+
+    /**
+     * Extract and remove the model ID from the given args.
+     *
+     * @return mixed any non-null ID value
+     */
+    public static function pullId(ArgumentSet $args, Model $model)
     {
         /** @var \Nuwave\Lighthouse\Execution\Arguments\Argument|null $id */
         $id = Arr::pull($args->arguments, 'id')
@@ -38,8 +52,25 @@ class UpdateModel implements ArgResolver
             throw new Error(self::MISSING_PRIMARY_KEY_FOR_UPDATE);
         }
 
-        $model = $model->newQuery()->findOrFail($id->value);
+        return $id->value;
+    }
 
-        return ($this->previous)($model, $args);
+    /**
+     * Extract and remove the model ID from the given args.
+     *
+     * @return mixed any non-null ID value
+     */
+    public static function getId(ArgumentSet $args, Model $model)
+    {
+        /** @var \Nuwave\Lighthouse\Execution\Arguments\Argument|null $id */
+        $id = Arr::get($args->arguments, 'id')
+            ?? Arr::get($args->arguments, $model->getKeyName())
+            ?? null;
+
+        if (null === $id) {
+            throw new Error(self::MISSING_PRIMARY_KEY_FOR_UPDATE);
+        }
+
+        return $id->value;
     }
 }
