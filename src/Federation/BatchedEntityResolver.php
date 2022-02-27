ee0a5f2432749bd07ec3c5efdbd95b7079ad16d6
spawnia <benedikt@franke.tech>
2022-02-27 11:53:51 +0100

    docs

--- src/Federation/BatchedEntityResolver.php
+++ src/Federation/BatchedEntityResolver.php
@@ -5,6 +5,8 @@ namespace Nuwave\Lighthouse\Federation;
 interface BatchedEntityResolver
 {
     /**
+     * Resolve multiple entities of a single type in one batch.
+     *
      * @param array<int, array<string, mixed>> $representations
      *
      * @return iterable<mixed> must be the same count as $representations
