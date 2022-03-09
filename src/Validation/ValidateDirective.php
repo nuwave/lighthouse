d6914404c282318e04825cdec0d45d9e6313631e
Benedikt Franke <benedikt.franke@mll.com>
2022-03-09 17:18:10 +0100

    normalize in RulesGatherer

--- src/Validation/ValidateDirective.php
+++ src/Validation/ValidateDirective.php
@@ -38,7 +38,7 @@ GRAPHQL;
                     assert($validationFactory instanceof ValidationFactory);
                     $validator = $validationFactory->make(
                         $args,
-                        self::normalizeRules($rulesGatherer->rules),
+                        $rulesGatherer->rules,
                         $rulesGatherer->messages,
                         $rulesGatherer->attributes
                     );
@@ -53,28 +53,5 @@ GRAPHQL;
                 }
             )
         );
-    }
-
-    /**
-     * Ensure rules follow the standard definition format that Laravel expects.
-     *
-     * @param  array<string, array<int, array<int, string>|object>>  $rulesMap
-     *
-     * @return array<string, array<int, string|object>>
-     */
-    private static function normalizeRules(array $rulesMap): array
-    {
-        foreach ($rulesMap as &$rulesForField) {
-            foreach ($rulesForField as &$rule) {
-                if (is_array($rule)) {
-                    $name = Arr::pull($rule, 0);
-                    $rule = count($rule) > 0
-                        ? $name . ':' . implode(',', $rule)
-                        : $name;
-                }
-            }
-        }
-
-        return $rulesMap;
     }
 }
