6fbbda3b00dc3445bc1199cba34f7c22b0e8bb3d
Carl <carl@dysfunctio.nl>
2022-03-22 16:32:28 +0700

    Update DropDirective definition description; also remove unwanted docblock

--- src/Schema/Directives/DropDirective.php
+++ src/Schema/Directives/DropDirective.php
@@ -3,7 +3,6 @@
 namespace Nuwave\Lighthouse\Schema\Directives;
 
 use Nuwave\Lighthouse\Schema\Values\FieldValue;
-use Nuwave\Lighthouse\Support\Contracts\FieldResolver;
 
 class DropDirective extends BaseDirective
 {
@@ -11,18 +10,12 @@ class DropDirective extends BaseDirective
     {
         return /** @lang GraphQL */ <<<'GRAPHQL'
 """
-Change the internally used name of a field or argument.
-
-This does not change the schema from a client perspective.
+Used to drop an argument or input field from the argument set.
 """
 directive @drop on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION
 GRAPHQL;
     }
 
-    /**
-     * @param FieldValue $fieldValue
-     * @return FieldValue
-     */
     public function resolveField(FieldValue $fieldValue): FieldValue
     {
         return $fieldValue->setResolver(
