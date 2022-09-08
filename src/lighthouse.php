2616e8a4848e3d5334e07f3c5adb75ac10942669
PK <pavkfr@hotmail.com>
2022-09-08 10:35:14 +0300

    feat(config): toggle disable_introspection from .env

--- src/lighthouse.php
+++ src/lighthouse.php
@@ -193,7 +193,9 @@ return [
     'security' => [
         'max_query_complexity' => \GraphQL\Validator\Rules\QueryComplexity::DISABLED,
         'max_query_depth' => \GraphQL\Validator\Rules\QueryDepth::DISABLED,
-        'disable_introspection' => \GraphQL\Validator\Rules\DisableIntrospection::DISABLED,
+        'disable_introspection' => (bool) env('LIGHTHOUSE_DISABLE_INTROSPECTION', false)
+            ? \GraphQL\Validator\Rules\DisableIntrospection::ENABLED
+            : \GraphQL\Validator\Rules\DisableIntrospection::DISABLED,
     ],
 
     /*
