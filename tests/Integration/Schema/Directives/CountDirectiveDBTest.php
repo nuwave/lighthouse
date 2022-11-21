768b28bf1467cd0c9792e7f641c1363fc135ecdf
bepsvpt <8221099+bepsvpt@users.noreply.github.com>
2022-11-21 21:21:02 +0800

    wip, fix tests

--- tests/Integration/Schema/Directives/CountDirectiveDBTest.php
+++ tests/Integration/Schema/Directives/CountDirectiveDBTest.php
@@ -78,7 +78,7 @@ final class CountDirectiveDBTest extends DBTestCase
     {
         $this->schema = /** @lang GraphQL */ '
         type Query {
-            users: [User!] @all
+            users: [User!] @all  @orderBy(column: "id")
         }
 
         type User {
@@ -94,23 +94,27 @@ final class CountDirectiveDBTest extends DBTestCase
             });
 
         $this->assertQueryCountMatches(2, function (): void {
-            $response = $this->graphQL(/** @lang GraphQL */ '
+            $this->graphQL(/** @lang GraphQL */ '
             {
                 users {
                     tasks_count
                 }
             }
-            ');
-
-            $response->assertJsonCount(3, 'data.users');
-
-            $users = $response->json('data.users');
-
-            $counts = collect($users)->pluck('tasks_count')->all();
-
-            sort($counts);
-
-            $this->assertEquals([1, 2, 3], $counts);
+            ')->assertExactJson([
+                'data' => [
+                    'users' => [
+                        [
+                            'tasks_count' => 3,
+                        ],
+                        [
+                            'tasks_count' => 2,
+                        ],
+                        [
+                            'tasks_count' => 1,
+                        ],
+                    ],
+                ],
+            ]);
         });
     }
 
