deabb67ed87f0d7050de6fd369182261c5c94756
Lyrisbee <Lyrisbee@users.noreply.github.com>
2022-11-22 16:23:06 +0800

    lint

--- src/Select/SelectHelper.php
+++ src/Select/SelectHelper.php
@@ -3,8 +3,8 @@
 namespace Nuwave\Lighthouse\Select;
 
 use GraphQL\Language\AST\DirectiveNode;
-use GraphQL\Language\AST\FieldDefinitionNode;
 use GraphQL\Language\AST\Node;
+use GraphQL\Language\AST\NodeList;
 use GraphQL\Language\AST\UnionTypeDefinitionNode;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
@@ -12,24 +12,39 @@ use Nuwave\Lighthouse\Schema\AST\ASTBuilder;
 use Nuwave\Lighthouse\Schema\AST\ASTHelper;
 use Nuwave\Lighthouse\Schema\AST\DocumentAST;
 use Nuwave\Lighthouse\Support\AppVersion;
+use Nuwave\Lighthouse\Support\Utils;
 
 class SelectHelper
 {
-    public const DirectivesRequiringLocalKey = ['hasOne', 'hasMany', 'count', 'morphOne', 'morphMany'];
+    public const DIRECTIVES_REQUIRING_LOCAL_KEY = ['hasOne', 'hasMany', 'count', 'morphOne', 'morphMany'];
 
-    public const DirectivesRequiringForeignKey = ['belongsTo'];
+    public const DIRECTIVES_REQUIRING_FOREIGN_KEY = ['belongsTo'];
 
-    public const DirectivesReturn = ['morphTo', 'morphToMany'];
+    public const DIRECTIVES_RETURN = ['morphTo', 'morphToMany'];
 
-    public const DirectivesIgnore = ['aggregate', 'withCount', 'belongsToMany'];
+    public const DIRECTIVES_IGNORE = ['aggregate', 'withCount', 'belongsToMany'];
+
+    public const DIRECTIVES = [
+        'aggregate',
+        'belongsTo',
+        'belongsToMany',
+        'count',
+        'hasOne',
+        'hasMany',
+        'morphOne',
+        'morphMany',
+        'morphTo',
+        'morphToMany',
+        'withCount',
+    ];
 
     /**
      * Given a field definition node, resolve info, and a model name, return the SQL columns that should be selected.
      * Accounts for relationships and to rename and select directives.
      *
-     * @param mixed[] $fieldSelection
+     * @param array<int, string> $fieldSelection
      *
-     * @return string[]
+     * @return array<int, string>
      *
      * @reference https://github.com/nuwave/lighthouse/pull/1626
      */
@@ -37,8 +52,9 @@ class SelectHelper
     {
         $returnTypeName = ASTHelper::getUnderlyingTypeName($definitionNode);
 
-        /** @var DocumentAST $documentAST */
         $documentAST = app(ASTBuilder::class)->documentAST();
+
+        assert($documentAST instanceof DocumentAST);
 
         if (Str::contains($returnTypeName, ['SimplePaginator', 'Paginator'])) {
             $returnTypeName = str_replace(['SimplePaginator', 'Paginator'], '', $returnTypeName);
@@ -50,11 +66,14 @@ class SelectHelper
             $type = $documentAST->types[ASTHelper::getUnderlyingTypeName($type->types[0])];
         }
 
-        /** @var iterable<FieldDefinitionNode> $fieldDefinitions */
         $fieldDefinitions = $type->fields;
+
+        assert($fieldDefinitions instanceof NodeList);
 
         /** @var Model $model */
         $model = new $modelName();
+
+        assert($model instanceof Model);
 
         $selectColumns = [];
 
@@ -62,42 +81,38 @@ class SelectHelper
             $fieldDefinition = ASTHelper::firstByName($fieldDefinitions, $field);
 
             if ($fieldDefinition) {
-                $directivesRequiringKeys = array_merge(self::DirectivesRequiringLocalKey, self::DirectivesRequiringForeignKey, self::DirectivesReturn, self::DirectivesIgnore);
-
-                foreach ($directivesRequiringKeys as $directiveType) {
+                foreach (self::DIRECTIVES as $directiveType) {
                     if (ASTHelper::hasDirective($fieldDefinition, $directiveType)) {
                         /** @var DirectiveNode $directive */
                         $directive = ASTHelper::directiveDefinition($fieldDefinition, $directiveType);
 
-                        if (in_array($directiveType, self::DirectivesReturn)) {
+                        if (in_array($directiveType, self::DIRECTIVES_RETURN)) {
                             return [];
                         }
 
-                        if (in_array($directiveType, self::DirectivesRequiringLocalKey)) {
+                        if (in_array($directiveType, self::DIRECTIVES_REQUIRING_LOCAL_KEY)) {
                             $relationName = ASTHelper::directiveArgValue($directive, 'relation', $field);
 
                             if (method_exists($model, $relationName)) {
-                                if (AppVersion::below(5.7)) {
-                                    $relation = $model->{$relationName}();
-                                    $rc = new \ReflectionClass($model->{$relationName}());
-                                    $localKey = $rc->getProperty('localKey');
-                                    $localKey->setAccessible(true);
-                                    array_push($selectColumns, $localKey->getValue($relation));
-                                } else {
-                                    array_push($selectColumns, $model->{$relationName}()->getLocalKeyName());
-                                }
+                                $relation = $model->{$relationName}();
+
+                                $localKey = AppVersion::below(5.7)
+                                    ? Utils::accessProtected($relation, 'localKey')
+                                    : $relation->getLocalKeyName();
+
+                                $selectColumns[] = $localKey;
                             }
                         }
 
-                        if (in_array($directiveType, self::DirectivesRequiringForeignKey)) {
+                        if (in_array($directiveType, self::DIRECTIVES_REQUIRING_FOREIGN_KEY)) {
                             $relationName = ASTHelper::directiveArgValue($directive, 'relation', $field);
 
                             if (method_exists($model, $relationName)) {
-                                if (AppVersion::below(5.8)) {
-                                    array_push($selectColumns, $model->{$relationName}()->getForeignKey());
-                                } else {
-                                    array_push($selectColumns, $model->{$relationName}()->getForeignKeyName());
-                                }
+                                $foreignKey = AppVersion::below(5.8)
+                                    ? $model->{$relationName}()->getForeignKey()
+                                    : $model->{$relationName}()->getForeignKeyName();
+
+                                $selectColumns[] = $foreignKey;
                             }
                         }
 
@@ -105,30 +120,23 @@ class SelectHelper
                     }
                 }
 
-                if (ASTHelper::hasDirective($fieldDefinition, 'select')) {
-                    // append selected columns in select directive to seletion
-                    $directive = ASTHelper::directiveDefinition($fieldDefinition, 'select');
-
-                    if ($directive) {
-                        $selectFields = ASTHelper::directiveArgValue($directive, 'columns') ?? [];
-                        $selectColumns = array_merge($selectColumns, $selectFields);
-                    }
-                } elseif (ASTHelper::hasDirective($fieldDefinition, 'rename')) {
+                if ($directive = ASTHelper::directiveDefinition($fieldDefinition, 'select')) {
+                    // append selected columns in select directive to selection
+                    $selectFields = ASTHelper::directiveArgValue($directive, 'columns', []);
+                    $selectColumns = array_merge($selectColumns, $selectFields);
+                } elseif ($directive = ASTHelper::directiveDefinition($fieldDefinition, 'rename')) {
                     // append renamed attribute to selection
-                    $directive = ASTHelper::directiveDefinition($fieldDefinition, 'rename');
-
-                    if ($directive) {
-                        $renamedAttribute = ASTHelper::directiveArgValue($directive, 'attribute');
-                        array_push($selectColumns, $renamedAttribute);
-                    }
+                    $renamedAttribute = ASTHelper::directiveArgValue($directive, 'attribute');
+                    $selectColumns[] = $renamedAttribute;
                 } else {
                     // fallback to selecting the field name
-                    array_push($selectColumns, $field);
+                    $selectColumns[] = $field;
                 }
             }
         }
 
-        $selectColumns = array_filter($selectColumns, function ($column) use ($model) {
+        /** @var array<int, string> $selectColumns */
+        $selectColumns = array_filter($selectColumns, function ($column) use ($model): bool {
             return ! $model->hasGetMutator($column) && ! method_exists($model, $column);
         });
 
