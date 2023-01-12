f0ec470a089c6f2a107494e3151e360dc0b83c08
Benedikt Franke <benedikt.franke@mll.com>
2023-01-12 12:19:36 +0100

    Cleanup routes.php

--- src/Support/Http/routes.php
+++ src/Support/Http/routes.php
@@ -7,6 +7,8 @@ use Nuwave\Lighthouse\Support\Http\Controllers\GraphQLController;
 $container = Container::getInstance();
 
 $config = $container->make(ConfigRepository::class);
+assert($config instanceof ConfigRepository);
+
 if ($routeConfig = $config->get('lighthouse.route')) {
     /**
      * Not using assert() as only one of those classes will actually be installed.
@@ -15,30 +17,30 @@ if ($routeConfig = $config->get('lighthouse.route')) {
      */
     $router = $container->make('router');
 
-    $actions = [
+    $action = [
         'as' => $routeConfig['name'] ?? 'graphql',
         'uses' => GraphQLController::class,
     ];
 
     if (isset($routeConfig['middleware'])) {
-        $actions['middleware'] = $routeConfig['middleware'];
+        $action['middleware'] = $routeConfig['middleware'];
     }
 
     if (isset($routeConfig['prefix'])) {
-        $actions['prefix'] = $routeConfig['prefix'];
+        $action['prefix'] = $routeConfig['prefix'];
     }
 
     if (isset($routeConfig['domain'])) {
-        $actions['domain'] = $routeConfig['domain'];
+        $action['domain'] = $routeConfig['domain'];
     }
 
     if (isset($routeConfig['where'])) {
-        $actions['where'] = $routeConfig['where'];
+        $action['where'] = $routeConfig['where'];
     }
 
     $router->addRoute(
         ['GET', 'POST'],
         $routeConfig['uri'] ?? 'graphql',
-        $actions
+        $action
     );
 }
