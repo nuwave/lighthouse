fc40332559edb4089f24cd87d165a92cfef54046
Benedikt Franke <benedikt.franke@mll.com>
2023-02-23 13:38:24 +0100

    Use `declare(strict_types=1)`
    https://github.com/nuwave/lighthouse/issues/2097

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-use function MLL\PhpCsFixerConfig\config;
+use function MLL\PhpCsFixerConfig\risky;
 
 $finder = PhpCsFixer\Finder::create()
     ->notPath('vendor')
@@ -9,4 +9,4 @@ $finder = PhpCsFixer\Finder::create()
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return config($finder);
+return risky($finder);
