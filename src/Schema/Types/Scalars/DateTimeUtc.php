1a5c96a5fca3c65c10e5e35d9b7abdbc7ad7d657
Benedikt Franke <benedikt.franke@mll.com>
2023-02-23 14:04:11 +0100

    Fix DateScalar types

--- src/Schema/Types/Scalars/DateTimeUtc.php
+++ src/Schema/Types/Scalars/DateTimeUtc.php
@@ -14,7 +14,7 @@ class DateTimeUtc extends DateScalar
         return $carbon->toJSON();
     }
 
-    protected function parse($value): Carbon
+    protected function parse(mixed $value): Carbon
     {
         // @phpstan-ignore-next-line We know the format to be good, so this can never return `false`
         return Carbon::createFromIsoFormat('YYYY-MM-DDTHH:mm:ss.SSSSSSZ', $value);
