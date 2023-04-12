f6fc58431144d97c5d75e07288794d7402681302
Benedikt Franke <benedikt.franke@mll.com>
2023-04-12 16:29:50 +0200

    comment for now, announce change for v7

--- src/lighthouse.php
+++ src/lighthouse.php
@@ -29,7 +29,7 @@ return [
          */
         'middleware' => [
             // Ensures the request is not vulnerable to cross-site request forgery.
-            Nuwave\Lighthouse\Http\Middleware\EnsureXHR::class,
+            // Nuwave\Lighthouse\Http\Middleware\EnsureXHR::class,
 
             // Always set the `Accept: application/json` header.
             Nuwave\Lighthouse\Http\Middleware\AcceptJson::class,
