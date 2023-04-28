08de9bf0161610fa356c4a4d907e7bc25b5f7392
Konstantin Duczek <konz.duczek@gmail.com>
2023-04-28 15:10:44 +0200

    also include relations in the builder argument for @aggregate

--- src/Schema/Directives/AggregateDirective.php
+++ src/Schema/Directives/AggregateDirective.php
@@ -8,7 +8,7 @@ use GraphQL\Language\AST\InterfaceTypeDefinitionNode;
 use GraphQL\Language\AST\ObjectTypeDefinitionNode;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Contracts\Database\Query\Builder as QueryBuilder;
 use Nuwave\Lighthouse\Exceptions\DefinitionException;
 use Nuwave\Lighthouse\Execution\BatchLoader\BatchLoaderRegistry;
 use Nuwave\Lighthouse\Execution\BatchLoader\RelationBatchLoader;
@@ -148,8 +148,8 @@ GRAPHQL;
                 $query = $builderResolver($root, $args, $context, $resolveInfo);
 
                 assert(
-                    $query instanceof QueryBuilder || $query instanceof EloquentBuilder,
-                    "The method referenced by the builder argument of the @{$this->name()} directive on {$this->nodeName()} must return a Builder.",
+                    $query instanceof QueryBuilder,
+                    "The method referenced by the builder argument of the @{$this->name()} directive on {$this->nodeName()} must return a Builder or Relation.",
                 );
 
                 $this->makeBuilderDecorator($root, $args, $context, $resolveInfo)($query);
