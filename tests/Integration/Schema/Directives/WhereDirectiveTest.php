dd1e259bab3759dab6bb343906389a0b61fa84f3
Missael H. Anda <31199350+MissaelAnda@users.noreply.github.com>
2023-09-28 14:25:26 -0600

    `@where` ignoreNull attribute (#2447)

--- tests/Integration/Schema/Directives/WhereDirectiveTest.php
+++ tests/Integration/Schema/Directives/WhereDirectiveTest.php
@@ -39,4 +39,47 @@ final class WhereDirectiveTest extends DBTestCase
             ')
             ->assertJsonCount(1, 'data.usersBeginningWithF');
     }
+
+    public function testIgnoreNull(): void
+    {
+        $userWithoutEmail = factory(User::class)->create(['email' => null]);
+        $userWithEmail = factory(User::class)->create();
+
+        $this->schema = /** @lang GraphQL */'
+        scalar DateTime @scalar(class: "Nuwave\\\Lighthouse\\\Schema\\\Types\\\Scalars\\\DateTime")
+
+        type User {
+            id: ID!
+            email: String
+        }
+
+        type Query {
+            usersIgnoreNull(email: String @where(ignoreNull: true)): [User!]! @all
+            usersExplicitNull(email: String @where): [User!]! @all
+        }
+        ';
+
+        $this
+            ->graphQL(/** @lang GraphQL */'
+            {
+                usersIgnoreNull(email: null) {
+                    id
+                }
+
+                usersExplicitNull(email: null) {
+                    id
+                }
+            }
+            ')
+            ->assertGraphQLErrorFree()
+            ->assertJsonCount(2, 'data.usersIgnoreNull')
+            ->assertJsonPath('data.usersIgnoreNull', [
+                ['id' => (string)$userWithoutEmail->id],
+                ['id' => (string)$userWithEmail->id],
+            ])
+            ->assertJsonCount(1, 'data.usersExplicitNull')
+            ->assertJsonPath('data.usersExplicitNull', [[
+                'id' => (string)$userWithoutEmail->id,
+            ]]);
+    }
 }
