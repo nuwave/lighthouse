7f241aa595b0c97f8f129773940c23ef25313a16
Benedikt Franke <benedikt.franke@mll.com>
2023-12-06 10:42:17 +0100

    Ensure CRUD model directives work

--- tests/Integration/Schema/Directives/NamespacedDirectiveTest.php
+++ tests/Integration/Schema/Directives/NamespacedDirectiveTest.php
@@ -6,69 +6,130 @@ use Tests\DBTestCase;
 
 final class NamespacedDirectiveTest extends DBTestCase
 {
-    public function testNamespaced(): void
+    public function testCRUDModelDirectives(): void
     {
         $this->schema .= /** @lang GraphQL */ '
+        type Query {
+            user: UserQueries! @namespaced
+        }
+
+        type UserQueries {
+            find(id: ID! @eq): User @find
+            list: [User!]! @all
+        }
+
         type Mutation {
             user: UserMutations! @namespaced
         }
 
         type UserMutations {
-            create(
-                name: String
-                tasks:  UserTasksOperations @nest
-            ): User @create
-        }
-
-        input UserTasksOperations {
-            newTask: CreateTaskInput @create(relation: "tasks")
-        }
-
-        input CreateTaskInput {
-            name: String
-        }
-
-        type Task {
-            name: String!
+            create(name: String!): User @create
+            update(id: ID!, name: String): User @update
+            delete(id: ID! @whereKey): User @update
         }
 
         type User {
-            name: String
-            tasks: [Task!]! @hasMany
+            id: ID!
+            name: String!
         }
         ';
 
-        $this->graphQL(/** @lang GraphQL */ '
-        mutation {
+        $name = 'foo';
+        $createUserResponse = $this->graphQL(/** @lang GraphQL */ '
+        mutation ($name: String!) {
             user {
-                create(
-                    name: "foo"
-                    tasks: {
-                        newTask: {
-                            name: "Uniq"
-                        }
-                    }
-                ) {
+                create(name: $name) {
+                    id
                     name
-                    tasks {
-                        name
-                    }
                 }
             }
         }
-        ')->assertExactJson([
+        ', [
+            'name' => $name,
+        ]);
+        $createUserResponse->assertJson([
             'data' => [
                 'user' => [
                     'create' => [
-                        'name' => 'foo',
-                        'tasks' => [
-                            [
-                                'name' => 'Uniq',
-                            ],
-                        ],
+                        'name' => $name,
                     ],
                 ],
             ],
+        ]);
+        $userID = $createUserResponse->json('data.user.create.id');
+
+        $this->graphQL(/** @lang GraphQL */'
+        query ($id: ID!) {
+            user {
+                find(id: $id) {
+                    id
+                }
+                list {
+                    id
+                }
+            }
+        }
+        ', [
+            'id' => $userID,
+        ])->assertExactJson([
+            'data' => [
+                'user' => [
+                    'find' => [
+                        'id' => $userID,
+                    ],
+                    'list' => [
+                        [
+                            'id' => $userID,
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+
+        $newName = 'bar';
+        $this->graphQL(/** @lang GraphQL */'
+        mutation ($id: ID!, $name: String) {
+            user {
+                update(id: $id, name: $name) {
+                    id
+                    name
+                }
+            }
+        }
+        ', [
+            'id' => $userID,
+            'name' => $newName,
+        ])->assertExactJson([
+            'data' => [
+                'user' => [
+                    'update' => [
+                        'id' => $userID,
+                        'name' => $newName,
+                    ],
+                ]
+            ]
+        ]);
+
+        $this->graphQL(/** @lang GraphQL */'
+        mutation ($id: ID!) {
+            user {
+                delete(id: $id) {
+                    id
+                    name
+                }
+            }
+        }
+        ', [
+            'id' => $userID,
+        ])->assertExactJson([
+            'data' => [
+                'user' => [
+                    'delete' => [
+                        'id' => $userID,
+                        'name' => $newName,
+                    ],
+                ]
+            ]
         ]);
     }
 }
