e7a0103bcb9bc391f3657bddd3368efe9065c1cc
Fabio Capucci <fabio.cappuc@gmail.com>
2023-12-11 11:23:36 +0100

    Add `scalar link__Import` and `enum link__Purpose` to `@link` definition

--- src/Federation/Directives/LinkDirective.php
+++ src/Federation/Directives/LinkDirective.php
@@ -22,6 +22,36 @@ extend schema @link(url: "https://specs.apollo.dev/federation/v2.3")
 https://www.apollographql.com/docs/federation/federated-types/federated-directives#the-link-directive
 """
 directive @link(url: String!, as: String, for: link__Purpose, import: [link__Import]) repeatable on SCHEMA
+
+"""
+An element to import into the document.
+
+```graphql
+@link(url: "https://specs.apollo.dev/link/v1.0", import: ["@link", "Purpose"])
+```
+
+or an object with name and (optionally as):
+
+```graphql
+@link(url: "https://specs.apollo.dev/link/v1.0", import: [{ name: "Purpose", as: "LinkPurpose" }])
+```
+"""
+scalar link__Import
+
+"""
+The role of a @linked schema.
+"""
+enum link__Purpose {
+    """
+    Provide metadata necessary to securely resolve fields.
+    """
+    SECURITY
+
+    """
+    Provide metadata necessary to correctly resolve fields.
+    """
+    EXECUTION
+}
 GRAPHQL;
     }
 }
