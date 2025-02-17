7002cff4ec4233ea6e8461b420afb6de71fee9eb
Benedikt Franke <benedikt.franke@mll.com>
2025-02-17 12:56:06 +0100

    fix more

--- src/Bind/BindDefinition.php
+++ src/Bind/BindDefinition.php
@@ -13,8 +13,8 @@ class BindDefinition
 {
     public function __construct(
         /** @var class-string<TClass> */
-        public string $class,
         // @phpstan-ignore generics.variance (TODO make class readonly)
+        public string $class,
         public string $column,
         /** @var array<string> */
         public array $with,
