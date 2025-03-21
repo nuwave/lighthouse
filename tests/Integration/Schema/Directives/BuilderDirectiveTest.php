efd9ad7564ad98389834992ef93a08954499ac70
Daniël van der Tang <daniel@cloudmedia.solutions>
2025-03-21 09:22:09 +0100

    Added test for specific decorator when paginating

--- tests/Integration/Schema/Directives/BuilderDirectiveTest.php
+++ tests/Integration/Schema/Directives/BuilderDirectiveTest.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
 use Nuwave\Lighthouse\Execution\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use Tests\DBTestCase;
+use Tests\Utils\Models\Task;
 use Tests\Utils\Models\User;
 
 final class BuilderDirectiveTest extends DBTestCase
@@ -155,6 +156,47 @@ final class BuilderDirectiveTest extends DBTestCase
         ')->assertJsonCount(2, 'data.users');
     }
 
+    public function testCallsCustomBuilderMethodOnFieldWithSpecificModel(): void
+    {
+        $this->schema = /** @lang GraphQL */ <<<GRAPHQL
+        type Query {
+            users: [User!]! @all
+        }
+
+        type User {
+            id: ID
+            tasks: [Task!]! @hasMany(type: SIMPLE) @builder(method: "{$this->qualifyTestResolver('specificModel')}")
+        }
+
+        type Task {
+            id: ID
+        }
+        GRAPHQL;
+
+        $users = factory(User::class, 2)->create();
+
+        foreach ($users as $user) {
+            $tasks = factory(Task::class, 3)->make();
+            $tasks[0]->name = $user->name;
+            $user->tasks()->saveMany($tasks);
+        }
+
+        $this->graphQL(/** @lang GraphQL */ '
+        {
+            users {
+                id
+                tasks(first: 10) {
+                    data {
+                        id
+                    }
+                }
+            }
+        }
+        ')
+            ->assertJsonCount(1, 'data.users.0.tasks.data')
+            ->assertJsonCount(1, 'data.users.1.tasks.data');
+    }
+
     /**
      * @param  \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder<\Tests\Utils\Models\User>  $builder
      *
@@ -163,5 +205,18 @@ final class BuilderDirectiveTest extends DBTestCase
     public static function limit(QueryBuilder|EloquentBuilder $builder, ?int $value): QueryBuilder|EloquentBuilder
     {
         return $builder->limit($value ?? 2);
+    }
+
+    /**
+     * @param  \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder<\Tests\Utils\Models\User>  $builder
+     *
+     * @return \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder<\Tests\Utils\Models\User>
+     */
+    public static function specificModel(
+        QueryBuilder|EloquentBuilder $builder,
+        ?int $value,
+        User $user,
+    ): QueryBuilder|EloquentBuilder {
+        return $builder->where('name', $user->name);
     }
 }
