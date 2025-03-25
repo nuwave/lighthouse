7a31a071d812f6157e31c5d4e5fd85a86dbc3451
Jérémy Gaulin <jeremy.gaulin@bnb.re>
2025-03-25 16:35:59 +0400

    Keep generics

--- src/Testing/MocksResolvers.php
+++ src/Testing/MocksResolvers.php
@@ -16,7 +16,7 @@ trait MocksResolvers
      *
      * @param  callable|mixed|null  $resolverOrValue
      *
-     * @return \PHPUnit\Framework\MockObject\Builder\InvocationMocker
+     * @return \PHPUnit\Framework\MockObject\Builder\InvocationMocker<\stdClass>
      */
     protected function mockResolver($resolverOrValue = null, string $key = 'default'): InvocationMocker
     {
@@ -36,7 +36,7 @@ trait MocksResolvers
      *
      * @param  \PHPUnit\Framework\MockObject\Rule\InvocationOrder  $invocationOrder
      *
-     * @return \PHPUnit\Framework\MockObject\Builder\InvocationMocker
+     * @return \PHPUnit\Framework\MockObject\Builder\InvocationMocker<\stdClass>
      */
     protected function mockResolverExpects(object $invocationOrder, string $key = 'default'): InvocationMocker
     {
