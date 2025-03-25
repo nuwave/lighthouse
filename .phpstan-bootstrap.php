08d0bb989c3f5b37d30747ecc1665201f2993e72
Jérémy Gaulin <jeremy.gaulin@bnb.re>
2025-03-25 12:24:10 +0400

    Fix PHPStan error Warning: Trying to access array offset on null src/Schema/AST/ASTCache.php:28

---
+++ .phpstan-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Illuminate\Container\Container;
+
+$existingConfig = Container::getInstance()->make('config');
+// Required by \Nuwave\Lighthouse\Schema\AST constructor when PHPStan analyzes code
+$existingConfig->set('lighthouse.schema_cache.enable', false);
