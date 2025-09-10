94f9b28ab5f18d430e5d768e342b848b6165556d
Benedikt Franke <benedikt.franke@mll.com>
2025-09-10 09:58:40 +0200

    control indentation and hash

--- tests/Integration/QueryCacheTest.php
+++ tests/Integration/QueryCacheTest.php
@@ -24,11 +24,11 @@ final class QueryCacheTest extends TestCase
 
         $event = Event::fake();
 
-        $this->graphQL(/** @lang GraphQL */ '
+        $this->graphQL(/** @lang GraphQL */ <<<'GRAPHQL'
         {
             foo
         }
-        ')->assertExactJson([
+        GRAPHQL)->assertExactJson([
             'data' => [
                 'foo' => Foo::THE_ANSWER,
             ],
@@ -39,11 +39,11 @@ final class QueryCacheTest extends TestCase
         $event->assertDispatchedTimes(KeyWritten::class, 1);
 
         // second request should be hit
-        $this->graphQL(/** @lang GraphQL */ '
+        $this->graphQL(/** @lang GraphQL */ <<<'GRAPHQL'
         {
             foo
         }
-        ')->assertExactJson([
+        GRAPHQL)->assertExactJson([
             'data' => [
                 'foo' => Foo::THE_ANSWER,
             ],
@@ -62,11 +62,11 @@ final class QueryCacheTest extends TestCase
 
         $event = Event::fake();
 
-        $this->graphQL(/** @lang GraphQL */ '
+        $this->graphQL(/** @lang GraphQL */ <<<'GRAPHQL'
         {
             foo
         }
-        ')->assertExactJson([
+        GRAPHQL)->assertExactJson([
             'data' => [
                 'foo' => Foo::THE_ANSWER,
             ],
@@ -96,11 +96,11 @@ final class QueryCacheTest extends TestCase
 
         $event = Event::fake();
 
-        $this->graphQL(/** @lang GraphQL */ '
+        $this->graphQL(/** @lang GraphQL */ <<<'GRAPHQL'
         {
             foo
         }
-        ')->assertExactJson([
+        GRAPHQL)->assertExactJson([
             'data' => [
                 'foo' => Foo::THE_ANSWER,
             ],
@@ -120,25 +120,25 @@ final class QueryCacheTest extends TestCase
         $config->set('lighthouse.query_cache.mode', 'opcache');
         $config->set('lighthouse.query_cache.opcache_path', $filesystem->path(''));
 
-        $expectedFilePath = 'lighthouse-query-1d4f5fbe8c2b4496ce01500ea3995524f819bc7665d03179125eef57a90e4a4a.php';
+        $expectedFilePath = 'lighthouse-query-ec859ac754fc185143d0daf8dcfa644b5e2271e219b9a8f80c3a6fdfb0ce67d0.php';
         $filesystem->assertMissing($expectedFilePath);
 
-        $this->graphQL(/** @lang GraphQL */ '
+        $this->graphQL(/** @lang GraphQL */ <<<'GRAPHQL'
         {
             foo
         }
-        ')->assertExactJson([
+        GRAPHQL)->assertExactJson([
             'data' => [
                 'foo' => Foo::THE_ANSWER,
             ],
         ]);
         $filesystem->assertExists($expectedFilePath);
 
-        $this->graphQL(/** @lang GraphQL */ '
+        $this->graphQL(/** @lang GraphQL */ <<<'GRAPHQL'
         {
             foo
         }
-        ')->assertExactJson([
+        GRAPHQL)->assertExactJson([
             'data' => [
                 'foo' => Foo::THE_ANSWER,
             ],
@@ -157,15 +157,15 @@ final class QueryCacheTest extends TestCase
 
         $event = Event::fake();
 
-        $expectedFilePath = 'lighthouse-query-1d4f5fbe8c2b4496ce01500ea3995524f819bc7665d03179125eef57a90e4a4a.php';
+        $expectedFilePath = 'lighthouse-query-ec859ac754fc185143d0daf8dcfa644b5e2271e219b9a8f80c3a6fdfb0ce67d0.php';
         $filesystem->assertMissing($expectedFilePath);
 
         // First request should miss both caches and create both store and file cache
-        $this->graphQL(/** @lang GraphQL */ '
+        $this->graphQL(/** @lang GraphQL */ <<<'GRAPHQL'
         {
             foo
         }
-        ')->assertExactJson([
+        GRAPHQL)->assertExactJson([
             'data' => [
                 'foo' => Foo::THE_ANSWER,
             ],
@@ -177,11 +177,11 @@ final class QueryCacheTest extends TestCase
         $event->assertDispatchedTimes(KeyWritten::class, 1);
 
         // Second request should hit file cache (no store cache events)
-        $this->graphQL(/** @lang GraphQL */ '
+        $this->graphQL(/** @lang GraphQL */ <<<'GRAPHQL'
         {
             foo
         }
-        ')->assertExactJson([
+        GRAPHQL)->assertExactJson([
             'data' => [
                 'foo' => Foo::THE_ANSWER,
             ],
@@ -203,12 +203,12 @@ final class QueryCacheTest extends TestCase
         $config->set('lighthouse.query_cache.mode', 'hybrid');
         $config->set('lighthouse.query_cache.opcache_path', $filesystem->path(''));
 
-        $query = /** @lang GraphQL */ '
+        $query = /** @lang GraphQL */ <<<'GRAPHQL'
         {
             foo
         }
-        ';
-        $queryHash = '1d4f5fbe8c2b4496ce01500ea3995524f819bc7665d03179125eef57a90e4a4a';
+        GRAPHQL;
+        $queryHash = 'ec859ac754fc185143d0daf8dcfa644b5e2271e219b9a8f80c3a6fdfb0ce67d0';
 
         $expectedFilePath = "lighthouse-query-{$queryHash}.php";
         $filesystem->assertMissing($expectedFilePath);
